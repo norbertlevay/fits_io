fef92032a2c368579a2714550267e4b2d912ddd2
Robert Butora <butora@oats.inaf.it>
2017-05-02 16:23:29 +0200

    Move Write_Header_To_New_FitsFile() to commands.adb

--- fitsfile.adb
+++ fitsfile.adb
@@ -1,7 +1,6 @@
 
 
 with Ada.Text_IO; -- for debug only
-with GNAT.OS_Lib;
 
 package body FitsFile is
 
@@ -402,65 +401,6 @@ package body FitsFile is
     Set_Index(HDU.FitsFile, HDU.Positions.Header_Index);
     Header_Type'Write(InOutFitsStreamAccess,Header);
   end Put;
-
-
- ---------------------------------------------
- ---------------------------------------------
- ---------------------------------------------
- ---------------------------------------------
- -- Move this to commands.ads:
- -- when sizes differ
- -- create new file and copy data there
- -- then rename the new file to orginal name
- procedure Write_Header_To_New_FitsFile ( InFitsFile : in File_Type;
-                                          HDU        : in HDU_Position_Type;
-                                          Header     : in String )
- is
-  OutFitsName : String := Name(InFitsFile) & ".part";
-  OutFitsFile : File_Type;
-  OutFitsStreamAccess  : Stream_Access;
-  InFitsStreamAccess   : Stream_Access := Stream(InFitsFile);
-
-  type Buffer_Type is new String(1 .. BlockSize);
-  Buffer : Buffer_Type;
-
-  Succeeded : Boolean := False;
- begin
-
-  Create (File => OutFitsFile,
-          Mode => Out_File,
-          Name => OutFitsName);
-  OutFitsStreamAccess := Stream(OutFitsFile);
-
-  -- copy from begining until Header starts
-  Set_Index(InFitsFile,1);
-  loop
-   exit when HDU.Header_Index = Index(OutFitsFile);
-   Buffer_Type'Read(InFitsStreamAccess,Buffer);
-   Buffer_Type'Write(OutFitsStreamAccess,Buffer);
-  end loop;
-
-  -- write new Header
-  String'Write(OutFitsStreamAccess,Header);
-  -- skip old Header
-  Set_Index(InFitsFile,HDU.Data_Index);
-
-  -- copy the rest after Header
-  while not End_OF_File(InFitsFile)
-  loop
-   Buffer_Type'Read( InFitsStreamAccess, Buffer);
-   Buffer_Type'Write(OutFitsStreamAccess,Buffer);
-  end loop;
-
-  Close(OutFitsFile);
-
-  -- rename <filename>.fits.part -> <filename>.fits
-  GNAT.OS_Lib.Rename_File (OutFitsName, Name(InFitsFile), Succeeded);
-  if not Succeeded then
-    null;
-    -- raise exception
-  end if;
- end Write_Header_To_New_FitsFile;
 
 end FitsFile;
 
