02e38c653d7f0f8c9ea046a87ac4627b30c4a37c
Robert Butora <butora@oats.inaf.it>
2017-05-02 17:52:04 +0200

    Hide some internal of FitsFile spec to private section. Backup writeHDU-with-copy.

--- commands.adb
+++ commands.adb
@@ -122,60 +122,6 @@ package body Commands is
  --
  -- write header to file
  --
- -- writes Header to position given by HDU
- -- this func always creates new temporary FitsFile assuming
- -- that size of Headers do not match and so Blocks
- -- need to be shifted. After succesful copy, temp file is
- -- renamed back to name of the original fits file
- procedure Write_Header_To_New_FitsFile ( InFitsFile : in File_Type;
-                                          HDU        : in HDU_Position_Type;
-                                          Header     : in String )
- is
-  OutFitsName : String := Name(InFitsFile) & ".part";
-  OutFitsFile : File_Type;
-  OutFitsStreamAccess  : Stream_Access;
-  InFitsStreamAccess   : Stream_Access := Stream(InFitsFile);
-
-  type Buffer_Type is new String(1 .. BlockSize);
-  Buffer : Buffer_Type;
-
-  Succeeded : Boolean := False;
- begin
-
-  Create (File => OutFitsFile,
-          Mode => Out_File,
-          Name => OutFitsName);
-  OutFitsStreamAccess := Stream(OutFitsFile);
-
-  -- copy from begining until Header starts
-  Set_Index(InFitsFile,1);
-  loop
-   exit when HDU.Header_Index = Index(OutFitsFile);
-   Buffer_Type'Read(InFitsStreamAccess,Buffer);
-   Buffer_Type'Write(OutFitsStreamAccess,Buffer);
-  end loop;
-
-  -- write new Header
-  String'Write(OutFitsStreamAccess,Header);
-  -- skip old Header
-  Set_Index(InFitsFile,HDU.Data_Index);
-
-  -- copy the rest after Header
-  while not End_OF_File(InFitsFile)
-  loop
-   Buffer_Type'Read( InFitsStreamAccess, Buffer);
-   Buffer_Type'Write(OutFitsStreamAccess,Buffer);
-  end loop;
-
-  Close(OutFitsFile);
-
-  -- rename <filename>.fits.part -> <filename>.fits
-  GNAT.OS_Lib.Rename_File (OutFitsName, Name(InFitsFile), Succeeded);
-  if not Succeeded then
-    null;
-    -- raise exception
-  end if;
- end Write_Header_To_New_FitsFile;
 
 
  procedure Write_Header( FitsFileName   : in String;
@@ -185,22 +131,21 @@ package body Commands is
    BitsInByte : Positive := 8;--FIXME get this from System.xxxx
    InFileHandle  : File_Type;
    OutFileHandle : File_Type;
-   HDU    : HDU_Position_Type;
+   -- HDU    : HDU_Position_Type;
    HeaderBlocks : String := Read_HeaderFromTextFile( HeaderFileName );
  begin
    Ada.text_IO.Put_Line("DBG: " & FitsFileName );
 
    -- first read where are HDUs
    Open(InFileHandle, In_File, FitsFileName );
-   HDU := Parse_HDU_Positions ( InFileHandle , HDU_Num );
+--   HDU := Parse_HDU_Positions ( InFileHandle , HDU_Num );
 
    -- now write
-   if Positive(HDU.Header_Size) = (HeaderBlocks'Size / BitsInByte) then
+--   if Positive(HDU.Header_Size) = (HeaderBlocks'Size / BitsInByte) then
 --    Write_Header ( InFileHandle, HDU, HeaderBlocks );
-    null;-- FIXME with Put()
-   else
-    Write_Header_To_New_FitsFile ( InFileHandle, HDU, HeaderBlocks );
-   end if;
+--   else
+--    Write_Header_To_New_FitsFile ( InFileHandle, HDU, HeaderBlocks );
+--   end if;
 
    Close(InFileHandle);
 
--- fitsfile.ads
+++ fitsfile.ads
@@ -8,29 +8,7 @@ package FitsFile is
  CardsCntInBlock : constant Positive := 36;
  BlockSize       : constant Positive := CardSize*CardsCntInBlock;
 
- type HDU_Position_Type is record
-  Header_Index : Positive_Count;
-  Header_Size  : Positive_Count;
-  Data_Index   : Positive_Count; -- FIXME there can be (primary?) HDU without DataUnit
-  Data_Size    : Positive_Count; -- FIXME Positive_Count is >0 !! Count is >=0 Ada.Stream.Stream_IO
- end record;
-
- HDU_Null : constant HDU_Position_Type := (1,1,1,1);
- -- FIXME Positive_Count does not allow 0, use Natural for size
-
- function Parse_HDU_Positions ( FitsFile : in File_Type; -- opened FitsFile
-                                HDU_Num  : in Positive ) -- which HDU: 1,2,3,...FIXME decide count from 0 or 1
-  return HDU_Position_Type;
-  -- get positions and sizes of HDU's in FitsFile
-
-
- -- HDU_Type based calls
-
- type HDU_Type is
-  record
-   FitsFile  : File_Type;
-   Positions : HDU_Position_Type;
-  end record;
+ type HDU_Type is limited private;
 
  procedure Create ( HDU : in out HDU_Type;
                     Mode : in File_Mode := Out_File;
@@ -51,8 +29,6 @@ package FitsFile is
 
  -- access Header
 
--- type Header_Type is array (Positive range <> ) of String(1..CardSize);
-
  type HeaderBlock_Type  is array (1 .. CardsCntInBlock) of String(1..CardSize);
  type HeaderBlocks_Type is array (Positive range <> ) of HeaderBlock_Type;
 
@@ -60,6 +36,26 @@ package FitsFile is
 
  procedure Put ( HDU          : in out HDU_Type;
                  HeaderBlocks : in HeaderBlocks_Type );
+
+
+
+private
+
+ type HDU_Position_Type is record
+  Header_Index : Positive_Count;
+  Header_Size  : Positive_Count;
+  Data_Index   : Positive_Count; -- FIXME there can be (primary?) HDU without DataUnit
+  Data_Size    : Positive_Count; -- FIXME Positive_Count is >0 !! Count is >=0 Ada.Stream.Stream_IO
+ end record;
+
+ HDU_Null : constant HDU_Position_Type := (1,1,1,1);
+ -- FIXME Positive_Count does not allow 0, use Natural for size
+
+ type HDU_Type is
+  record
+   FitsFile  : File_Type;
+   Positions : HDU_Position_Type;
+  end record;
 
 end FitsFile;
 
