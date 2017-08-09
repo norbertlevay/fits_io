74b6f1ba594d4914dcd31fbf0e1a3ae58323ddee
RBu <robert.butora@kolumbus.fi>
2017-08-09 18:42:59 +0200

    Block_IO block-index calcs are private.

--- fits_io-block_io.ads
+++ fits_io-block_io.ads
@@ -10,29 +10,30 @@ package FITS_IO.Block_IO is
 
  package SIO renames Ada.Streams.Stream_IO;
 
+ function  To_SIO( Mode : in FITS_File_Mode ) return SIO.File_Mode;
+
  BlockSize       : constant Positive_Count := 2880; -- [FITS, Sect 3.1]
  CardsCntInBlock : constant Positive_Count := BlockSize / Positive_Count(CardSize);
  -- [FITS 3.3.1 Primary Header] 36 cards per block        -- conv ok, CardSize 80 const
 
+ -- Card
+
  subtype Card_Type is String(1..CardSize);
  -- makes sure index start with 1
+
+ ENDCard    : constant Card_Type := "END                                                                             ";
+ EmptyCard  : constant Card_Type := (others => ' ');
+
+ -- Header Block
+
  type HeaderBlock_Type is array (1 .. CardsCntInBlock) of Card_Type;
  type HeaderBlockArray_Type is array (Positive_Count range <>) of HeaderBlock_Type;
  -- Header format inside FITS-file
 
- ENDCard    : constant Card_Type := "END                                                                             ";
- EmptyCard  : constant Card_Type := (others => ' ');
  EmptyBlock : constant HeaderBlock_Type := (others => EmptyCard);
-
- function  To_SIO( Mode : in FITS_File_Mode ) return SIO.File_Mode;
 
  -- positioning in file
 
- function  To_BlockIndex( OctetIndex : in  Positive_Count ) return Positive_Count;
- function  To_OctetIndex( BlockIndex : in  Positive_Count ) return Positive_Count;
-
- -- Fits_IO.Count inhereted from SIO.Count:
- -- Index() and Set_Index() are inherited from SIO.[Set_]Index()
  function  BlockIndex ( File  : in SIO.File_Type ) return Positive_Count;
 
  procedure Set_BlockIndex ( File  : in SIO.File_Type;
