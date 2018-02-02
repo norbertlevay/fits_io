1ec08e2925a9aa6c71632c9aff5b02f747d4404b
rbu <robert.butora.fi@gmail.com>
2018-02-02 14:30:10 +0200

    PNG 4.6 build tested

--- png/rbu-git-clone-libz.sh
+++
@@ -1,8 +0,0 @@
-
-git clone https://aur.archlinux.org/libz.git 
-
-# This is an ArchLinux/AUR pack:
-# makepkg -si stops with conflict error: libz conflicts zlib
-# ok -> so I did not install, kept the compiled binaries and 
-# linked zlib-ada (and so png_4_6) against them.
-
