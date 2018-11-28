431ea060126bb4dfcaf3d099df71cf856ba36029
rbu <robert.butora.fi@gmail.com>
2018-11-29 00:00:47 +0100

    Add FITS.Keyword class hierarchy

---
+++ lib/src/fits-keyword.ads
@@ -0,0 +1,29 @@
+
+with FITS.Header; use FITS.Header;
+
+
+package FITS.Keyword is
+
+
+   type Keyword_Type is tagged
+    record
+	Name    : Max_8.Bounded_String;
+    end record;
+
+    function Match(Key  : in Keyword_Type;
+                   Card : in Card_Type) return Boolean;
+
+
+   type Indexed_Keyword_Type is new Keyword_Type with
+    record
+        Index_First : Natural;
+        Index_Last  : Positive;
+    end record;
+
+   function Parse_Index(Key : in Indexed_Keyword_Type) return Natural;
+
+   function Match(Key  : in Indexed_Keyword_Type;
+                  Card : in Card_Type) return Boolean;
+
+end FITS.Keyword;
+
