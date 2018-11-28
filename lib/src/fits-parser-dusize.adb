1b86906d7dd0deee8d6e0cab85b0e9670a72036c
rbu <robert.butora.fi@gmail.com>
2018-11-28 16:20:09 +0100

    Parser: add constant array of Keys to parse from Header

--- lib/src/fits-parser-dusize.adb
+++ lib/src/fits-parser-dusize.adb
@@ -1,6 +1,14 @@
 
+with FITS.Header; use FITS.Header;
 
 package body FITS.Parser.DUSize is
+
+   -- holds array of keywords which will be parsed
+   DU_Size_Keys : constant array (Positive range <>) of Key := (
+    (Max_8.To_Bounded_String("BITPIX"), False, 0,   1, False),
+    (Max_8.To_Bounded_String("NAXIS"),  False, 0,   1, False),
+    (Max_8.To_Bounded_String("NAXIS"),  True,  1, 999, False)
+   );
 
 
    -- in PType use List to collect NAXIS vector elements
@@ -10,26 +18,50 @@ package body FITS.Parser.DUSize is
     -- l : List; -- list element instead of NAXIS_Arr vector
    end record;
 
-   function NAXISList_Length(PData : in PType) return Natural
-   is
-   begin
-    return 1;
-   end NAXISList_Length;
-
    type UType is record
     dummy : Positive;
    end record;
 
+   PData  : PType;
+   UData  : UType;
+   -- FIXME put both to Heap or Global-mem
+
+   -- internally uses DU_Size_Keys array
    function Parse_Card_DUSize
                     (Card      : in     Card_Type;
                      PData     : in out PType;
                      UData     : in out UType)
                      return Boolean
    is
+    CardKey : Max_8.Bounded_String := Max_8.To_Bounded_String(Card(1..8));
+    CurKey  : Key;
    begin
+
+    for I in DU_Size_Keys'Range
+    loop
+      CurKey := DU_Size_Keys(I);
+      if (CurKey.Indexed) then
+        if(Max_8.To_String(CurKey.Name) = Max_8.To_String(CardKey)) then
+           -- convert Key Index and Card value and store
+           -- them into PData dynamic array (List or Container.Vector)
+           null;
+        end if;
+      else -- pure Key
+        if(Max_8.To_String(CurKey.Name) = Max_8.To_String(CardKey)) then
+           -- convert Card value to PData value
+           null;
+        end if;
+      end if;
+    end loop;
+
     return False;
    end Parse_Card_DUSize;
 
+   function NAXISList_Length(PData : in PType) return Natural
+   is
+   begin
+    return 1;
+   end NAXISList_Length;
 
    -- first parse IndexedKeys into a List
    -- read length and provide it to DU_Size_Type(Length)
@@ -40,11 +72,8 @@ package body FITS.Parser.DUSize is
                           Parse_Card  => Parse_Card_DUSize);
 
 
-
    function Parse_To_List return Natural
    is
-     PData  : PType;
-     UData  : UType; -- FIXME put both to Heap
      Source : FITS.Parser.Source_Type;
    begin
     -- actual Parsing happens here
@@ -53,13 +82,17 @@ package body FITS.Parser.DUSize is
    end Parse_To_List;
 
 
+   ---------------
+   -- interface --
+   ---------------
+
    function Parse_DUSize(Source : in Source_Type)
      return DU_Size_Type
      is
       Len : Positive := Parse_To_List;
       DUSize : DU_Size_Type(Len);
      begin
-      -- convert from List to array
+      -- convert from List to array and fill in DUSize
       return DUSize;
      end Parse_DUSize;
 
--- lib/src/fits-parser.ads
+++ lib/src/fits-parser.ads
@@ -1,11 +1,24 @@
 
 with FITS; use FITS; -- Card_Block type
+with FITS.Header; use FITS.Header; -- Max_8 type
 
 generic
    type Source_Type is private;
    with function Next(Source : Source_Type) return Card_Block is <>;
    -- source can be fits-file, memory (list of cards), network etc...
 package FITS.Parser is
+
+   -- FIXME consider variable record for Key types:
+   -- Key, Indexed_Key, Variant Key
+   type Key is
+    record
+     Name    : Max_8.Bounded_String; -- keyword string
+     Indexed : Boolean;
+     Min     : Natural;
+     Max     : Positive;
+--     Variant : Character;
+     Found   : Boolean;
+    end record;
 
    -- position Source_Type to begining of the Header
    -- before calling this
