544772ac45c6edc86cc0ed9df182f6ae9774f595
rbu <robert.butora.fi@gmail.com>
2018-12-05 12:06:19 +0100

    Parser: added XTENSION to DUSize parser - later find better solution.

--- lib/src/fits-parser-dusize.adb
+++ lib/src/fits-parser-dusize.adb
@@ -45,16 +45,22 @@ package body FITS.Parser.DUSize is
         declare
          Name : String := Max_8.To_String(KeyRec.Name);
         begin
-         if(Name = "BITPIX") then
+
+         if(Name = "XTENSION" OR Name = "SIMPLE") then
+           DUSize.XTENSION := KeyRec.Value;
+
+         elsif(Name = "BITPIX") then
            DUSize.BITPIX := Integer'Value(Max20.To_String(KeyRec.Value));
+
          elsif ( Name(1..5) = "NAXIS" ) then
 
           if( Name'Length > 5 ) then
              Idx := Natural'Value( Name(6..Name'Last) );
              DUSize.NAXISArr(Idx) := Natural'Value(Max20.To_String(KeyRec.Value));
-           end if;
+          end if;
 
          end if;
+
         end;
 
         Out_Key_List.Next(CurPos);
@@ -66,15 +72,17 @@ package body FITS.Parser.DUSize is
    function Init_List return In_Key_List.List
    is
     InKeys :  In_Key_List.List;
-    simple_ptr : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("SIMPLE"));
-    bitpix_ptr : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("BITPIX"));
-    naxis_ptr  : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("NAXIS"));
-    naxisarr_ptr  : Keyword_Ptr := new Indexed_Keyword_Type'(Name => Max_8.To_Bounded_String("NAXIS"),
+    simple_ptr   : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("SIMPLE"));
+    xtension_ptr : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("XTENSION"));
+    bitpix_ptr   : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("BITPIX"));
+    naxis_ptr    : Keyword_Ptr := new Keyword_Type'(Name => Max_8.To_Bounded_String("NAXIS"));
+    naxisarr_ptr : Keyword_Ptr := new Indexed_Keyword_Type'(Name => Max_8.To_Bounded_String("NAXIS"),
                                                            Index_First =>  1,
                                                            Index_Last  =>999,
                                                            Index       =>0);
    begin
     InKeys.Append(simple_ptr);
+    InKeys.Append(xtension_ptr);
     InKeys.Append(bitpix_ptr);
     InKeys.Append(naxis_ptr);
     InKeys.Append(naxisarr_ptr);
