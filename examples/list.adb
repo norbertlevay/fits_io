6c1db0c42d9d5304726027178968f8df2b994d03
Norbert Levay <norbert.levay@gmail.com>
2019-01-26 13:22:21 +0100

    Example list: now triggers FITS_IO.File.Set_Index. Not listing HDU_Info yet. Used for testing FITS_IO.

--- examples/list.adb
+++ examples/list.adb
@@ -5,74 +5,79 @@
 --
 
 
-with Ada.Text_IO;      use Ada.Text_IO;
-with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO;      --use Ada.Text_IO;
+with Ada.Command_Line; --use Ada.Command_Line;
 
 with Ada.Exceptions;   use Ada.Exceptions;
 with GNAT.Traceback.Symbolic;
 
-with Ada.Streams.Stream_IO;
+with Ada.Streams.Stream_IO;  --use Ada.Streams.Stream_IO;
 
-with FITS_IO;        use FITS_IO;
-with FITS_IO.Header; use FITS_IO.Header;
-with FITS_IO.File;   use FITS_IO.File;
 
+with FITS_IO.File;   --use FITS_IO.File;
+with Ada.Strings.Unbounded;
 
 procedure list
 is
 
- package SIO renames Ada.Streams.Stream_IO;
  package TIO renames Ada.Text_IO;
+ package SIO renames Ada.Streams.Stream_IO;
+ package CLI renames Ada.Command_Line;
+ package SU  renames Ada.Strings.Unbounded;
+ package FIO renames FITS_IO.File;
 
- InFile      : SIO.File_Type;
- InFileName  : String := Argument(1);
- -- FIXME might raise excpetion before Usage written
-
-
- procedure Read_Data  is new gen_Read_Data (Data_Arr => UInt8_Arr);
- procedure Write_Data is new gen_Write_Data(Data_Arr => Float32_Arr);
-
- BITPIXFloat64Card : Card_Type :=
-   To_Card (Max_8.To_Bounded_String("BITPIX"),
-            Max20.To_Bounded_String("-32"),
-            Max48.To_Bounded_String("Float 32 Data Type"));
+ InFileName : SU.Unbounded_String; 
+ InFile     : SIO.File_Type;
+ HDUNum     : Positive := 1;
 
 begin
 
- Put_Line("Usage  " & Command_Name & " <file name>");
+ if (CLI.Argument_Count /= 1) then
+   TIO.Put_Line("Usage  " & CLI.Command_Name & " <file name>");
+   return;
+ else
+   InFileName := SU.To_Unbounded_String (CLI.Argument (1));
+ end if;
 
- SIO.Open   (InFile,  SIO.In_File,  InFileName);
 
- -- interpret header: DataUnit length and type needed
- declare
-  HDUInfo : HDU_Info_Type := Get(InFile);
- begin
-  BITPIX := HDUInfo.BITPIX;
-  DUSize := DU_Size (HDUInfo.NAXISn);
- end;
 
- SIO.Close(OutFile);
- SIO.Close(InFile);
+   SIO.Open(InFile, SIO.In_File, SU.To_String(InFileName));
 
- Put_Line("done.");
+   while not SIO.End_Of_File(InFile)
+   loop
+     declare
+       -- FIXME tbd later: HDUInfo : HDU_Info := FITS_IO.File.List.Get(InFile);
+     begin
+       TIO.Put_Line(Positive'Image(HDUNum));
+       HDUNum := HDUNum + 1;
+       FIO.Set_Index(InFile, HDUNum);
+     end;
+   end loop;
+
+   SIO.Close(InFile);
+
 
 
  exception
 
   when Except_ID : others =>
      declare
-      Error : Ada.Text_IO.File_Type := Standard_Error;
+      Error : TIO.File_Type := TIO.Standard_Error;
      begin
-      New_Line(Error);
-      Put_Line(Error, "Exception_Information: ");
-      Put_Line(Error, Exception_Information( Except_ID ) );
-      New_Line(Error);
-      Put_Line(Error, "Call stack traceback symbols: addr2line -e ./fits addr1 addr2 ...");
-      Put_Line(" > Trace-back of call stack: " );
-      Put_Line( GNAT.Traceback.Symbolic.Symbolic_Traceback(Except_ID) );
+      TIO.Put_Line(Error, Exception_Message( Except_ID ) );
+      TIO.New_Line(Error);
+      TIO.Put_Line(Error, Exception_Information( Except_ID ) );
+      TIO.Put_Line(Error, "Call stack traceback symbols: addr2line -e ./list -a addr1 addr2 ...");
+
+      -- TIO.Put_Line(Error, " > Trace-back of call stack: " );
+      -- TIO.Put_Line(Error, GNAT.Traceback.Symbolic.Symbolic_Traceback(Except_ID) );
       -- See more at: http://compgroups.net/comp.lang.ada/gnat-symbolic-traceback-on-exceptions/1409155#sthash.lNdkTjq6.dpuf
       -- Do the same manually, use:
       -- addr2line -e ./fits -a addr1 addr2 ...
      end;
 end list;
+
+   
+   
+
 
