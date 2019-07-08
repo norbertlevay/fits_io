85f216472ff6beddb2e2a517f04ce1ee06560da2
Robert Butora <robert.butora@inaf.it>
2019-07-08 07:42:03 +0200

    FITSlib comment/notes only.

--- lib/src/fitslib-hdu.ads
+++ lib/src/fitslib-hdu.ads
@@ -1,13 +1,18 @@
 --
--- HDU may be "too big" to keep in memory. Source_Type is "container" holding the HDU; usually
+-- Multi-HDU FITS: Many HDU's are stored in one File. This module describes access (Read/Write) 
+-- to one particular HDU.
+--
+-- HDU may be "too big" to keep in memory. Source/Sink_Type is "container" holding the HDU; usually
 -- file on a disk. This module operates on buffer.
 --
+-- FIXME is this really needed? or use directéy .File ads? (back in old FITS with only one HDU this would not be needed HDU was Media(File) itself, but now with Extensions: e.g. Prim + N-extensions[+ unspec datablocks] in one File, this might reflect multi-HDU files better??)    Then File would have ops like Set_HDU, Copy_HDU, Remove_HDU...
 -- TODO add generic param N to configure buffer size (N-blocks)
 
 with FITSlib.Header; use FITSlib.Header;
 
 generic
  Type  Source_Type is limited private;
+ Type  Sink_Type   is limited private;
  with function Next (Source : Source_Type) return Card_Block;
 package FITSlib.HDU is
 
@@ -33,7 +38,7 @@ package FITSlib.HDU is
 	
 	function Read_Header (Source : Source_Type) return HDU_Type;
 	
-	procedure Write_Header (Source : Source_Type; HDUType : HDU_Type) is null;
+	procedure Write_Header (Sink : Sink_Type; HDUType : HDU_Type) is null;
 
 
 
