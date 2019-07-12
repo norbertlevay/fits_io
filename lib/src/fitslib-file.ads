43ec808ccb996d1912678ec6884dc448ca7f52bb
Robert Butora <robert.butora@inaf.it>
2019-07-12 22:22:51 +0200

    FITSlib File vs HDU division: ads spec-file adjustmens and notes/comments.

--- lib/src/fitslib-file.ads
+++ lib/src/fitslib-file.ads
@@ -1,9 +1,34 @@
-
+--
+-- FITS File contains a set of HDU's. This module provides operations with HDUs,
+-- within one file or betwen two files.
+--
+-- (Operations _within_ HDU like get/set properties, Read/Write data, are in HDU module.)
+--
 
 with Ada.Streams.Stream_IO;
 
 package FITSlib.File is
 
    package SIO renames Ada.Streams.Stream_IO;
+
+
+
+   procedure Set_HDU
+	   (File   : SIO.File_Type; 
+	    HDUNum : Positive) is null;
+
+
+   -- and also something like:
+
+   procedure Remove_HDU
+	   (File   : SIO.File_Type; 
+	    HDUNum : Positive) is null;
+
+   procedure Copy_HDU
+	   (FromFile   : SIO.File_Type; 
+	    FromHDUNum : Positive;
+	    ToFile     : SIO.File_Type; 
+	    ToHDUNum   : Positive) is null;
+
 
 end FITSlib.File;
--- lib/src/fitslib-hdu.ads
+++ lib/src/fitslib-hdu.ads
@@ -1,29 +1,24 @@
 --
--- HDU spec
+-- HDU modules provides properties of HDU like type, size, data dimensions...
+-- and Read/Write access to Header and Data _within_ the selected HDU.
+-- (Use File.Set_HDU to select one particular HDU.)
 --
--- FIXME is this really needed? or use directéy .File ads? 
--- (back in old FITS with only one HDU this would not be needed HDU was 
--- Media(File) itself, but now with Extensions: e.g. 
--- Prim + N-extensions[+ unspec datablocks] in one File, 
--- this might reflect multi-HDU files better??)
--- Then File would have ops like Set_HDU, Copy_HDU, Remove_HDU...
+-- NOTE both (File and HDU) operate on File_Type/Source_Type
+-- and provide buffered access.
+-- 
+-- HDU module uses info (type records) specified in Headers to calculate 
+-- HDU properties.
 --
--- File -> HDU separation allows to separate 
--- operations _within_ HDU(Read/Write/Header/Data) vs 
--- operations _with_ HDUs (remove HDU, copy HDU,..).
--- HOWEVER both (File and HDU) operate on File_Type !
+-- HDU may be "too big" to keep in memory. Source/Sink_Type 
+-- is "container" holding the HDU; usually file on a disk. 
+--
+-- Multi-HDU FITS: many HDU's are stored in one File. 
+-- This module describes access (Read/Write) to one particular HDU.
 --
 -- ALTERNATIVE: don't separate, have only File and
 -- disctiguish ops with HDU vs on HDU by function names (prefix HDU_):
 -- Remove_HDU(File, HDUNum) vs HDU_Read_Primary_Header(File, PrimHead)
 --
--- Multi-HDU FITS: Many HDU's are stored in one File. This module describes access (Read/Write) 
--- to one particular HDU.
---
--- HDU may be "too big" to keep in memory. Source/Sink_Type is "container" holding the HDU; usually
--- file on a disk. This module operates on buffer.
---
--- FIXME add generic param N to configure buffer size (N-blocks)
 -- FIXME _consider_ implement one common DataDimensions_Type and 
 -- DataSize calculation routine for all three types below.
 -- Use it also for list() of HDU info --> e.g. how to map
@@ -34,52 +29,20 @@ with FITSlib.Header; use FITSlib.Header;
 
 
 generic
--- NBuuferBlocks : Positive := 1;
+ NBufferBlocks : Positive := 1;-- FIXME not implemented
  type  Buffered_Source_Type is limited private;
  type  Buffered_Sink_Type   is limited private;
  with function Next_Buffer_Content (Source : Buffered_Source_Type) return Card_Block;
 package FITSlib.HDU is
 
-	--FIXME id FirstBlock needed - review for other solution
+	-- --------------------
+	-- HDU properties
+	-- -------------------- 
 
-	procedure Read_Conforming_Extensions
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block;
-		 HEnd       : out HeaderSize_Type;
-                 ConfExt    : out Conforming_Extension_Type);
+	-- FIXME Read_ or Get_ ?? File-Index is moved(?).
 
-	procedure Read_Random_Groups
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block;
-		 HEnd       : out HeaderSize_Type;
-		 RandGroups : out Random_Groups_Type);
-
-	procedure Read_Primary 
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block;
-		 HEnd       : out HeaderSize_Type;
-                 PrimImg    : out Primary_Image_Type);
-
-
-
-	function Read_Conforming_Extensions_Data_Size_bits 
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block) return Natural;
-	
-	function Read_Random_Groups_Data_Size_bits 
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block) return Natural;
-		 
-	function Read_Primary_Data_Size_bits
-		(Source     : Buffered_Source_Type;
-                 FirstBlock : Card_Block) return Natural;
-
-
-
-	-- read header (for all known HDU types)
-
-	function Read_Data_Size_bits (Source : Buffered_Source_Type) return Natural;
-
+	function Read_Data_Size_bits
+		(Source : Buffered_Source_Type) return Natural;
 
 
 	type Data_Dimensions_Type is
@@ -97,25 +60,13 @@ package FITSlib.HDU is
    
 
 
-	-- writing routines for varios data types ...
-	
-	procedure Write_Primary_Image 
-		(Sink : Buffered_Sink_Type; 
-		 Primary : Primary_Image_Type) is null;
-
-
-	-- -------------------------------------------------------------------------
 	-- experimental with generic Read_Cards
-	-- -------------------------------------------------------------------------
-	
-        procedure Read_Exp 
-		(File : Buffered_Source_Type; 
-		 DDims : out Data_Dimensions_Type);
-	
 	procedure Read_Exp 
 		(File : Buffered_Source_Type; 
 		 DSize : out Natural);
- 
-
+	procedure Read_Exp 
+		(File : Buffered_Source_Type; 
+		 DDims : out Data_Dimensions_Type);
+	
 
 end FITSlib.HDU;
