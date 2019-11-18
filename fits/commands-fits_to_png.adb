1cd4ff3dd83371fb51e6b334f6cf09c07c492102
Robert Butora <robert.butora@inaf.it>
2019-11-18 14:08:19 +0100

    Eliminates fits-data (had only find-MinMax routine for fits-to-png)

--- fits/commands-fits_to_png.adb
+++ fits/commands-fits_to_png.adb
@@ -4,14 +4,12 @@ with Ada.Text_IO,
      Ada.Unchecked_Deallocation,
      FITS,
      FITS.File,
-     FITS.Data,
      System,
      Ada.Streams.Stream_IO;
 
 use
      FITS,
      FITS.File,
-     FITS.Data,
      System,
      Ada.Streams.Stream_IO;
 
@@ -283,6 +281,36 @@ use  Interfaces;
    end loop;
 
  end Convert_FITS_Float32_To_PNG_16bit;
+
+
+   -- find minimum and maximum value of the Float32 data array
+   procedure Find_MinMax_Float32
+--              (F32Arr : in  Float32Arr_Type;
+              (F32Arr : in  Float32_Arr;
+               Min    : out Float_32;
+               Max    : out Float_32)
+   is  
+     type MyFloat is new Float_32;
+     D   : FITS.Float_32;
+   begin
+
+     Min := Float_32'Large;
+     Max := Float_32'Small;
+
+     --for D of F32Arr  Ada2012 feature
+     for ix in F32Arr'Range
+      loop
+
+       D := F32Arr(ix);
+
+       if D > Max then Max := D; end if;
+       if D < Min then Min := D; end if;
+
+     end loop;
+
+   end Find_MinMax_Float32;
+
+
 
 
  procedure Convert_FITS_Float32_To_PNG_24rgb
