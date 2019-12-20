960066f6eac1d76a14608eb69b5d1609e07eba9b
Robert Butora <robert.butora@inaf.it>
2019-12-20 16:51:44 +0100

    Cosmetic, clean fits.ads comments

--- lib/src/fits.ads
+++ lib/src/fits.ads
@@ -1,10 +1,10 @@
 --
--- FITS.* packages
+-- FITS-lib packages in lib/src
 --
 -- Media management:
 --
--- FITS.File child-package: helps to do file management
--- for FITS-files, positioning in FITS-file, list content etc...
+-- File package: does file management for FITS-files:
+-- positioning in FITS-file, list content etc...
 -- It buids on Ada.Streams.Stream_IO and so many
 -- procedures accept Ada.Stream_IO.File_Type and File_Mode
 -- as parameter.
@@ -12,32 +12,14 @@
 --
 -- Data Access:
 --
--- FITS.Header FITS.Data packages:
+-- parser/Keqword_Record Image packages:
 -- Once Ada-file Stream is successfully acquired,
 -- serialization/deserialization into/from stream
 -- happens simply by using Read Write attributes of
--- the predefined types, specifically FitsData_Type'Read and 'Write,
+-- the predefined types, specifically <FitsData_Type>'Read and 'Write,
 -- which is array of base Ada-types and so supported by
 -- Ada GNAT implementation [FITS ?][GNAT ?].
 --
--- FIXME Note: currently only PrimaryHeader & Image extension supported.
--- Table Extensions to be added.
--- Parse_Card recognizes only mandatory keywords for primary header which
--- are essentially the same as for IMAGE extension [FITS Sect 7.1, Table 13].
--- Each extension-type has its own set of mandatory keywords.
--- [FITS Sect 4.4.1.2 Conforming Extensions]: Extensions have
--- PCOUNT & GCOUNT keys, but except of BINTABLE (where PCOUNT/=0)
--- their value is 0 & 1 respectively, and so yield (Eq2) the same
--- DU size as Primary haeder (Eq1).
--- PCOUNT & GCOUNT were meant for Random Groups [FITS Section 6]
---
-
---with System.Storage_Elements; use System.Storage_Elements;
-
-with Interfaces;
-with Ada.Streams.Stream_IO;
-
-with Keyword_Record; use Keyword_Record; -- FInteger needed
 
 package FITS is
 
