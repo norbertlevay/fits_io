6f9ec9b6b160028350ab926f9d68e71fc22fc382
Robert Butora <robert.butora@inaf.it>
2020-01-28 14:16:59 +0100

    Adds script to download FITS test-example files (from NRAO web site)

---
+++ data/rbu-wget-nrao.edu-examples.sh
@@ -0,0 +1,24 @@
+wget -r -l 1 https://www.cv.nrao.edu/fits/data/tests/ftt4b
+wget -r -l 1 https://www.cv.nrao.edu/fits/data/tests/nost_headers
+wget -r -l 1 https://www.cv.nrao.edu/fits/data/tests/incunabula
+wget -r -l 1 https://www.cv.nrao.edu/fits/data/tests/pg93
+
+
+# https://www.cv.nrao.edu/fits/data/tests/README.html
+
+#This directory tree contains collections of FITS files which test various features of the FITS data format:
+
+# ftt4b 
+# -- FITS Test Tape 4b, a collection of FITS files produced in the early 80s in order to test FITS readers and demonstrate the versatility of FITS as a format.
+
+# headers
+# -- A collection of 'FITS' files produced by Barry Schlesinger which test various syntactic errors of headers.
+
+#incunabula
+# -- A collection of the oldest FITS files known, including a copy of the first FITS file which passed in interchange between two observatories.
+
+#pg93
+# -- A collection of test files produced by Preben Grosbol in order to test FITS readers for implementation of support for the full set of IEEE cases (infinities, NaNs, etc) and to test support for the proposed BINTABLE and IMAGE extensions.
+
+
+
