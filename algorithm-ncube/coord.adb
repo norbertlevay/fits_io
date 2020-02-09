f8fadecf5a6789e2bdae7c2fcaf69dcebbc9cdcb
Robert Butora <robert.butora@inaf.it>
2020-02-09 22:25:42 +0100

    Algorithms NCube: adds to previous linear offset calc

--- algorithm-ncube/coord.adb
+++ algorithm-ncube/coord.adb
@@ -7,21 +7,64 @@ N : Integer := 3;
 
 type Coord_Arr is array (Positive range 1 .. N) of Integer;
 
-procedure print_coord(C : in Coord_Arr) is
+ function To_Offset (Coords    : in  Coord_Arr;
+                     MaxCoords : in  Coord_Arr)
+   return Positive
+ is
+  Offset : Positive;
+  Sizes  : Coord_Arr := MaxCoords;
+ begin
+  if Coords'Length /= MaxCoords'Length
+  then
+   null;
+   -- raise exception <-- needed this if ?
+   -- no, check only high level inputs, this is not direct API call
+   -- assume if code corrct, it is corrct here
+  end if;
+
+  --  
+  -- generate size of each plane
+  --  
+  declare
+    Accu  : Positive := 1;
+  begin
+    for I in MaxCoords'First .. (MaxCoords'Last - 1)
+    loop
+     Accu := Accu * MaxCoords(I);
+     Sizes(I) := Accu;
+     -- FIXME Acc is not needed, init Sizes(1):=1 and use Sizes
+    end loop;
+  end;
+
+  Offset := Coords(1);
+  for I in (Coords'First + 1) .. Coords'Last
+  loop
+   Offset := Offset + (Coords(I) - 1) * Sizes(I - 1); 
+  end loop;
+
+  return Offset;
+ end To_Offset;
+
+
+
+procedure print_coord(C : in Coord_Arr)
+is
+M : Coord_Arr := (10,10,10); 
 begin
 for I in C'Range loop
 Put(Integer'Image(C(I)) & " ");
 end loop;
+Put(" : "& Integer'Image(To_Offset(C,M)));
 New_Line;
 end print_coord;
 
 
 
-
-
+-- --------------------------------------------------------
+-- coord generate algorithm variables
 W : Integer := 1;
 
-F :  Coord_Arr := (3,1,5);
+F :  Coord_Arr := (3,1,7);
 L :  Coord_Arr := (5,1,9);
 
 I : Integer := 1;
