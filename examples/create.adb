8ba6fb55448bd572895777dc11e6bf612a00edb9
Robert Butora <robert.butora@inaf.it>
2020-02-25 00:46:32 +0100

    examples/create: 1st shot at write mandatory result_rec as cards

--- examples/create.adb
+++ examples/create.adb
@@ -17,36 +17,92 @@ with Keyword_Record; use Keyword_Record; -- FPositive needed
 with Mandatory; use Mandatory; -- Positive_Arr needed
 with Optional; use Optional; -- Card_Arr & ENDCard needed 
 
+-- NEW BEGIN
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+-- NEW END
+
 procedure create
 is
 
  package TIO renames Ada.Text_IO;
  package SIO renames Ada.Streams.Stream_IO;
- use SIO; -- NOTE bacause 'operator not visible'
+ use SIO; -- NOTE because 'operator not visible'
 
  FileName : constant String := Command_Name & ".fits";
  File     : SIO.File_Type;
+
+
+------------------------------------------------------------------------------------------
+-- BEGIN this should go to lib
+
+function Create_Mandatory_Card(Key : in String; Value : in String) return String_80
+is
+    C : String(1 .. 80);
+begin
+    Move(Key,   C(1 .. 8));
+    Move("= ",  C(9 ..10));
+    Move(Value, C(11..30));
+    Move(" ",   C(31..80));
+    return C;
+end Create_Mandatory_Card;
+
+
+
+function To_Value_String( V : in Integer) return String
+is
+    Vstr: String(1 .. 20);
+begin
+    Move(Integer'Image(V), Vstr, Error, Right);
+    return Vstr;
+end To_Value_String;
+
+
+
+function To_Value_String( V : in Boolean) return String
+is
+    Vstr : String(1 .. 20);
+begin
+    if(V = True) then
+        Move("T", Vstr, Error, Right);
+    else
+        Move("F", Vstr, Error, Right);
+    end if;
+    return Vstr;
+end To_Value_String;
+
+
+-- END this should go to lib
+------------------------------------------------------------------------------------------
+
+
 
  -- Describe the Data
 
  RowsCnt : constant SIO.Positive_Count := 500;-- = ColumnLength
  ColsCnt : constant SIO.Positive_Count := 500;-- = Row   Length
 
--- MaxCoords : constant Positive_Arr := (FPositive(RowsCnt),ColsCnt);
+-- FIXME lessons learned: 
+-- below: TFIELDS_Last and CardsCount should not be required to be given
+Mand : Mandatory.Result_Rec
+    := (HDU =>IMAGE, NAXIS_Last => 2, TFIELDS_Last => 0,
+        CardsCount => 8, BITPIX => -32, NAXISn => (RowsCnt, ColsCnt));
 
- -- Prepare the Header
+-- FIXME there should be function to convert Result_Rec -> Card_Arr
 
- -- Card => Key Value Comment
- Cards : Card_Arr :=  (
-"SIMPLE  =                    T / Standard FITS FIle                             ",
-"BITPIX  =                  -32 /                                                ",
-"NAXIS   =                    2 /                                                ",
-"NAXIS1  =                  500 /                                                ",
-"NAXIS2  =                  500 /                                                ",
-"DATAMIN =                    0 /                                                ",
-"DATAMAX =                  255 /                                                ",
-   ENDCard
-   );
+Cards : Card_Arr(1 .. Positive(Mand.CardsCount)) -- FIXME conversion
+    := (
+    Create_Mandatory_Card("SIMPLE",  To_Value_String(True)),
+    Create_Mandatory_Card("BITPIX",  To_Value_String(Mand.BITPIX)),
+    Create_Mandatory_Card("NAXIS",   To_Value_String(Natural(Mand.NAXIS_Last))),-- FIXME
+    Create_Mandatory_Card("NAXIS1",  To_Value_String(Positive(Mand.NAXISn(1)))),-- FIXME
+    Create_Mandatory_Card("NAXIS2",  To_Value_String(Positive(Mand.NAXISn(2)))),-- FIXME convs
+    Create_Mandatory_Card("DATAMIN", To_Value_String(0)),
+    Create_Mandatory_Card("DATAMAX", To_Value_String(255)),
+    ENDCard
+    );
+
+
 
 
  function SomeData(OffInDU : SIO.Positive_Count) return Float_32
@@ -62,6 +118,15 @@ is
  NDataElems : constant SIO.Positive_Count := RowsCnt*ColsCnt;
 
 begin
+
+-- NEW BEGIN
+for I in Cards'Range
+loop
+    Put_Line("DBG NEW >"& Cards(I) & "<");
+end loop;
+
+
+-- NEW END
 
  Put_Line("Usage  " & Command_Name );
  Put("Writing " & FileName & " ... ");
