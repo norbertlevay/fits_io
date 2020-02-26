7306c655ad068343c9eac07d9e46da1bffaab523
Robert Butora <robert.butora@inaf.it>
2020-02-26 22:47:16 +0100

    Image: only adds notes

--- lib/src/image/image.ads
+++ lib/src/image/image.ads
@@ -1,4 +1,24 @@
 
+-- NOTE 
+-- conversions: V3_Types -> Value-string (both: fixed- & free-format)
+-- write padding (?when EndCard written?)
+-- or introduce some OpenHDU/CreateHDU - CloseHDU:
+-- @Open/Create write Mandatory, @Close write ENDCard & padding (or padding only?)
+--
+-- Loweset level:
+-- allow writing whatever card (?) or even whatever String_80
+-- allow writing non-standard header (SIMPLE=F)
+-- allow writig standard header (SIMPLE=T)
+-- allow writing extensions (XTENSION=...)
+--
+-- Higher Level:
+-- Image_Rec'Write
+-- Reserved_Biblio_Card_Arr'Write
+-- Reserved_Image_Card_Arr'Write
+-- ...
+-- Optional_Card_Arr'Write : write proprietary cards not defined by Standard
+-- ...
+
 with Mandatory; use Mandatory; -- NAXISn_Arr needed
 with Optional; use Optional; -- Card_Arr & ENDCard needed
 
