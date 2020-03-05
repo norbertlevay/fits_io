62f6434d39e5bb25b7b586ab990730f67ca7402a
Robert Butora <robert.butora@inaf.it>
2020-03-05 23:14:00 +0100

    Data_Value: remove Scale with BLANK (BLANK will be scaled also)

--- lib/src/data/data_unit.ads
+++ lib/src/data/data_unit.ads
@@ -100,6 +100,25 @@ generic
         First  : in Positive := 1);
 
 
+
+ -- new trial
+ generic
+    with procedure Element_Value(V : in Tout);
+    with procedure Invalid is <>;
+ procedure Read_Scaled_Values
+        (F : SIO.File_Type; 
+        Length : in Positive_Count;
+        BZERO  : in Tout;
+        BSCALE : in Tout;
+        Undef_Val : in Tout;
+        First  : in Positive := 1) is null;
+
+
+
+
+
+
+
 end Physical;
 
 
