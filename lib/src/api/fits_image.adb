da722bb1db999f1b89cb9878143e2ac9040d76c7
Robert Butora <robert.butora@inaf.it>
2020-03-30 22:51:43 +0200

    api: added body skeleton of FITS_Image (does not compile)

--- lib/src/api/fits_image.adb
+++ lib/src/api/fits_image.adb
@@ -2,72 +2,91 @@
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;-- Positive_Count needed or is there FITS.Positive_count?
 with Mandatory; use Mandatory;-- NAXIS_Arr needed
 
-package FITS_Image is
-  
-  package SIO renames Ada.Streams.Stream_IO;
-
-
--- internal FIST_Image model: type, undefined value, dimensions
-
--- Read metadata
+package body FITS_Image is
 
 procedure Read_Dimensions
   (F : in SIO.File_Type;
   BITPIX : out Integer;
-  NAXISn : out NAXIS_Arr);
--- NOTE comes from Mandatory-keys parsing
+  NAXISn : out NAXIS_Arr)
+is
+begin
+  null;
+end Read_Dimensions;
 
-generic
-  type Tm is private;
-function Read_Undef_Value(F : in SIO.File_Type) return Tm;
--- returns Scaled(BLANK) or Float_NaN
--- NOTE comes from Optional-keys parsing
+
+function Read_Undef_Value(F : in SIO.File_Type) return Tm
+is
+  Vm : Tm;
+begin
+  return Vm;
+end Read_Undef_Value;
 
 
 -- Read data
 
-generic
-  type Tm is private;
-  type Tm_Arr is array (Positive_Count range <>) of Tm;
 procedure Read_Plane
   (F : in SIO.File_Type;
   Undef_Value : in Tm;   -- FIXME how to give "none" no BLANK provided (for raw Integers data)
   NAXISi : in NAXIS_Arr; -- dimensions of Tm_Arr  (i < NAXISn'Length):  Length of Plane Tm_Arr
-  Plane : out Tm_Arr);
+  Plane : out Tm_Arr)
+is
+  BITPIX : Integer;
+  NAXISn : NAXIS_Arr(1..1);-- FIXME
+
+  procedure ReadIntPlane   is new Read_Int_Plane(Tf, Tm, Tm_Arr, Tc);
+  procedure ReadFloatPlane is new Read_Float_Plane(Tf, Tm, Tm_Arr, Tc, Undefi_Value);
+begin
+  Read_Dimensions(F, BITPIX, NAXISn);
+
+  if(BITPIX > 0)
+  then
+    -- call ReadIntPlane()
+    null;
+  else
+    -- call ReadFloatPlane()
+    null;
+  end if;
+
+end Read_Plane;
 
 
-generic
-  type Tm is private;
-  type Tm_Arr is array (Positive_Count range <>) of Tm;
 procedure Read_Volume
   (F : in SIO.File_Type;          -- File F
   HDUStart : in Positive_Count;   -- at offset FDUStart (referenced by HDUNum)
   NAXISn : in NAXIS_Arr;          -- contains n-dimensional array (n=NAXISn'Length)
   Undef_Value : in Tm;
   First, Last : in NAXIS_Arr;     -- F and L point limiting the subcube of NAXISn
-  Plane : out Tm_Arr);
+  Plane : out Tm_Arr)
+is
+begin
+  null;
+end Read_Volume;
 
 
 
 -- Write,  BITPIX, Undef_Value, NAXISn are known
 
-generic
-  type Tm is private;
-  type Tm_Arr is array (Positive_Count range <>) of Tm;
 procedure Write_Plane
   (F : in SIO.File_Type;
-  Plane : in Tm_Arr);
+  Plane : in Tm_Arr)
+is
+begin
+  null;
+end Write_Plane;
 
 
-generic
-  type Tm is private;
-  type Tm_Arr is array (Positive_Count range <>) of Tm;
+
+
 procedure Write_Volume
   (F : in SIO.File_Type;
   HDUStart : in Positive_Count;
   NAXISn : in NAXIS_Arr;
   First, Last : in NAXIS_Arr;
-  Plane : in Tm_Arr);
+  Plane : in Tm_Arr)
+is
+begin
+  null;
+end Write_Volume;
 
 
 end FITS_Image;
