75f193a5474144ca980e8c8e6c6a7c74fbf3648a
Robert Butora <robert.butora@inaf.it>
2020-04-30 23:39:42 +0200

    scaling: modifies NaN check, adds comments and example code: ieee.adb

--- lib/src/data/unit.adb
+++ lib/src/data/unit.adb
@@ -131,17 +131,33 @@ procedure Write_Array
  end Scale;
 
 
+-- NOTE See also 'Machine_Overflows True/False in conjuction with
+ -- floating point behaviour  ->  whether NaN Inf will be generated as 
+ -- result of float operations or some exception raised
+ -- GNAT switches like -mieee and -gnato?? affect exception (and also some pragmas?)
+
+ -- How to test for NaN:
+ -- example from Rosetta Code (And IEEE-Standard says) shows: NaN = NaN is FALSE, 
+ -- but PInf = Pinf is TRUE as for any other number
+
+ -- unverified from iNet:
+ -- Catching NaN requires a three-way test, viz.:
+ -- if Value /= 0.0 and then (not (Value < 0.0)) and then (not (Value > 0.0))
+ -- then
+ --   <Value is NaN>;
+ -- end if;
 
 
 
  function Scale_Float(Vf : in Tf) return Tm
  is
  begin
- if(Vf'Valid)
+ --if(Vf'Valid) -- evaluates false besides NaN also for +/-Inf
+ if(Vf = Vf) -- true for any number except NaN NOTE make sure compiler does not optimize it out
  then
     return +( BZERO + BSCALE * (+Vf) );
  else
-    return Undef;
+    return Undef;-- retun NaN in Tm type
  end if;
  end Scale_Float;
 
