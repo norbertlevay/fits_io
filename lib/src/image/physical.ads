6cd1f2ae4c8dfe877acf8a02f90a945a750f46b6
Robert Butora <robert.butora@inaf.it>
2020-05-05 11:24:39 +0200

    image: replaces Undef param value with Is_NaN() function param; adds comments

--- lib/src/image/physical.ads
+++ lib/src/image/physical.ads
@@ -2,14 +2,25 @@
  -- FIXME  later make T_Arr private and also include
  -- NAXISn BITPIX and BLANK/UndefValue
 
+-- NOTE:
+-- with given set of actuals for Tm Tc Tf, compiler can automatically
+-- substitute the actuals of funcs in generic-params if those are available:
+-- * example: for V3 types prepare V3_Types.Linear V3_Types.Conversions
+-- * and caller at instantiating Physical can include V3_Types.*
+-- * e.g. caller can simply write:
+-- *
+-- * package F32I16_Physical is new Physical(Float_32, Long_Float, Integer_16);
+
+
+
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 with Mandatory; use Mandatory; -- NAXIS_Arr needed
 
- generic
-  type Tf is private;
-  type Tm is private;
-  type Tc is digits <>;
-  Undef : in Tm; -- returns this when Vin invalid
+generic
+  type Tm is private;       -- type in memory (data is returned to caller in this type)
+  type Tc is digits <>;     -- type in which scaling is calculated
+  type Tf is private;       -- type in fits-file;
+  with function Is_NaN(Vin : in Tf) return Boolean is <>;
   with function Linear(BZERO,BSCALE : in Tc; Vin : in Tf) return Tm is <>;
   with function "+"(R : in Tf) return Tc is <>;
   with function "+"(R : in Tc) return Tm is <>;
@@ -23,7 +34,7 @@ package Physical is
   -- sequential access
 
 
-procedure Read_Array
+ procedure Read_Array
    (F : SIO.File_Type;
     BZERO, BSCALE : in Tc;
     Data : out Tm_Arr);
