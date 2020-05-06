8238610cc4a3f35d860362cc93830df8a3a89fcb
Robert Butora <robert.butora@inaf.it>
2020-05-06 17:18:10 +0200

    image: adds undefined values (BLANK/NaN) handling into Linear_Conv

--- lib/src/image/linear_conv.adb
+++ lib/src/image/linear_conv.adb
@@ -3,22 +3,64 @@
 
 package body Linear_Conv is
 
--- NOTES
-    -- Vin = Vin is check for NaN: for NaN results of False
--- FIXME
-    -- how to return NaN for a given instantiation ? --> we need _generic_NaN_ which gets resolved to F32NaN F64NaN ... at instantiation.
-    -- how many actual NaN's exist? IEEE float standard defines 2 F64NaN F32NaN how about 128bit NaN ???
-    -- e.g. whichever float-type FITSlib wll support, such float-type must have defined NaN-value
-    -- -> example put NaN in V3_Types and user includes V3_Types at instatiation -> NaN gets resolved
-    -- ONE SOLUTION: put NaN as generic parameter with enabled default and compiler will puul it in from Vn_Types
+-- FIXME make better names
 
-function FF(A,B:Tc; Vin : Tin) return Tout is begin if(Vin = Vin) then return Tout(A+B*Tc(Vin)); else return ToutNaN; end if; end FF;--FIXME Tout(0.0) should be Tout(NaN)
+-- NOTE Vin = Vin is check for NaN: for NaN result is False
+
+-- FIXME
+    -- cases which involve Float have to deal with NaN as mark for undefined values
+    -- FF -> simple: if(NaN) -> Tout(NaN) and Vn_Types will procide NaN defienitions
+
+    -- FI FU & no   BLANK from Header -> simply scale, no check
+    -- FI FU & with BLANK from Header -> if(BLANK) -> Tout(NaN)
+
+    -- IF UF -> what BLANK to use if NaN encoutered in Vin ? -> Ask user to provide BLANK (he must know the data-range, should be able to provide BLANK
+    -- IF UF -> no   BLANK -> user did not give BLANK, but NaN found -> raise exception "Data contains undefined values (NaN) but no BLANK given"
+    -- IF UF -> with BLANK -> if(NaN) -> return BLANK & check that no value after conversion results in BLANK: if(Vout=BLANK) raise exception "BLANK among valid values"
+
+function FF(A,B:Tc; Vin : Tin) return Tout is begin if(Vin = Vin) then return Tout(A+B*Tc(Vin)); else return ToutNaN; end if; end FF;
 function FI(A,B:Tc; Vin : Tin) return Tout is begin return Tout(A+B*Tc(Vin)); end FI;
 function FU(A,B:Tc; Vin : Tin) return Tout is begin return Tout(A+B*Tc(Vin)); end FU;
+function FI_BLANK(BLANK: Tin; A,B:Tc; Vin : Tin) return Tout is begin if(Vin = BLANK) then return ToutNaN; else return Tout(A+B*Tc(Vin)); end if; end FI_BLANK;
+function FU_BLANK(BLANK: Tin; A,B:Tc; Vin : Tin) return Tout is begin if(Vin = BLANK) then return ToutNaN; else return Tout(A+B*Tc(Vin)); end if; end FU_BLANK;
 
-function rF(A,B:Tc; Vin : Tin) return Tout is begin return Tout(A+B*Tc(Vin)); end rF;
 
--- FIXME make better names
+
+
+function rF(A,B:Tc; Vin : Tin) return Tout
+is
+begin
+    if(Vin = Vin)
+    then
+        return Tout(A+B*Tc(Vin));
+    else
+        return Tout'First; -- FIXME raise Exception "UndefVal found but no BLANK provided"
+    end if; 
+end rF;
+
+
+
+
+    function rF_NaN(BLANK: Tout; A,B:Tc; Vin : Tin) return Tout
+    is 
+        V: Tout;
+    begin 
+        if(Vin = Vin)
+        then 
+            V:=Tout(A+B*Tc(Vin));
+            if(V=BLANK)
+            then
+                null; -- FIXME raise exception "BLANK found among valid values"
+            end if;
+            return V;
+        else 
+            return BLANK;
+        end if;
+    end rF_NaN;
+
+
+
+
 
 
 end Linear_Conv;
--- lib/src/image/linear_conv.ads
+++ lib/src/image/linear_conv.ads
@@ -4,10 +4,23 @@
 package Linear_Conv is
 
 
-generic type Tin is digits <>; type Tc is digits <>; type Tout is digits <>; ToutNaN : Tout;  function FF(A,B: Tc; Vin : Tin) return Tout;
-generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>; function FI(A,B: Tc; Vin : Tin) return Tout;
-generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>; function FU(A,B: Tc; Vin : Tin) return Tout;
+-- convert to Floats
+
+generic type Tin is digits <>; type Tc is digits <>; type Tout is digits <>; ToutNaN : Tout; function FF(A,B: Tc; Vin : Tin) return Tout;
+generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>; ToutNaN : Tout; function FI_BLANK(BLANK: Tin; A,B: Tc; Vin : Tin) return Tout;
+generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>; ToutNaN : Tout; function FU_BLANK(BLANK: Tin; A,B: Tc; Vin : Tin) return Tout;
+generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>;                 function FI(A,B: Tc; Vin : Tin) return Tout;
+generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>;                 function FU(A,B: Tc; Vin : Tin) return Tout;
+
+
+-- convert to Ints
 
 generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>; function rF(A,B: Tc; Vin : Tin) return Tout;
+generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>; function rF_NaN(BLANK: Tout; A,B: Tc; Vin : Tin) return Tout;
+
+
+-- convert to UInts
+
+
 
 end Linear_Conv;
