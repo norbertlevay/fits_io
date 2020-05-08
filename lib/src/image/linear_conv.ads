efed9bfa38e6716fa6ba5c8c15445b162e1495a4
Robert Butora <robert.butora@inaf.it>
2020-05-08 22:52:56 +0200

    image: fixes bug where BZERO BSCALE and BLANK were not in-out generic-params, so they took the value at instantiation, but not later

--- lib/src/image/linear_conv.ads
+++ lib/src/image/linear_conv.ads
@@ -6,17 +6,17 @@ package Linear_Conv is
 
 -- convert to Floats
 
-generic type Tin is digits <>; type Tc is digits <>; type Tout is digits <>;             A,B: Tc; ToutNaN : Tout; function FF(Vin : Tin) return Tout;
-generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>; BLANK: Tin; A,B: Tc; ToutNaN : Tout; function FI_BLANK(Vin : Tin) return Tout;
-generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>; BLANK: Tin; A,B: Tc; ToutNaN : Tout; function FU_BLANK(Vin : Tin) return Tout;
-generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>;             A,B: Tc;                 function FI(Vin : Tin) return Tout;
-generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>;             A,B: Tc;                 function FU(Vin : Tin) return Tout;
+generic type Tin is digits <>; type Tc is digits <>; type Tout is digits <>;                    A,B: in out Tc; ToutNaN : Tout; function FF(Vin : Tin) return Tout;
+generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>; BLANK: in out Tin; A,B: in out Tc; ToutNaN : Tout; function FI_BLANK(Vin : Tin) return Tout;
+generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>; BLANK: in out Tin; A,B: in out Tc; ToutNaN : Tout; function FU_BLANK(Vin : Tin) return Tout;
+generic type Tin is range  <>; type Tc is digits <>; type Tout is digits <>;                    A,B: in out Tc;                 function FI(Vin : Tin) return Tout;
+generic type Tin is mod    <>; type Tc is digits <>; type Tout is digits <>;                    A,B: in out Tc;                 function FU(Vin : Tin) return Tout;
 
 
 -- convert to Ints
 
-generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>;              A,B: Tc; function rF(Vin : Tin) return Tout;
-generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>; BLANK: Tout; A,B: Tc; function rF_NaN(Vin : Tin) return Tout;
+generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>;                     A,B: in out Tc; function rF(Vin : Tin) return Tout;
+generic type Tin is digits <>; type Tc is digits <>; type Tout is range <>; BLANK: in out Tout; A,B: in out Tc; function rF_NaN(Vin : Tin) return Tout;
 
 
 -- convert to UInts
