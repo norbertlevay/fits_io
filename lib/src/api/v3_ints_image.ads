72a363cc989bf14b129a96a9918aeb100d3f0fe4
Robert Butora <robert.butora@inaf.it>
2020-05-10 11:39:27 +0200

    api: adds V3_Ints_Image module and adds accordingly image_data sub-packages for integers

---
+++ lib/src/api/v3_ints_image.ads
@@ -0,0 +1,51 @@
+
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;-- Positive_Count needed
+with Mandatory; use Mandatory;-- NAXIS_Arr needed
+
+with V3_Types; use V3_Types;
+
+generic
+type Tm is range <>;
+type Tm_Arr is array (Positive_Count range <>) of Tm;
+type Tcalc is digits <>;
+package V3_Ints_Image is
+
+  package SIO renames Ada.Streams.Stream_IO;
+
+
+
+generic
+TmNaN : Tm;
+procedure Read_Volume
+    (F : in SIO.File_Type;
+    HDUStart : in Positive_Count;
+    First, Last : in NAXIS_Arr;
+    Volume : out Tm_Arr);
+
+
+procedure Write_Volume
+    (F : in SIO.File_Type;
+    HDUStart : in Positive_Count;
+    First, Last : in NAXIS_Arr;
+    NAXISn : in NAXIS_Arr;
+    Volume : in Tm_Arr);
+
+
+
+generic
+TmNaN : Tm;
+with procedure Plane_Data(Plane : in Tm_Arr; PlaneCount : in Positive_Count);
+procedure Read_Data_Unit_By_Planes
+    (F : in SIO.File_Type;
+    NAXISi : in NAXIS_Arr); -- Tm_Arr has size NAXIS1 .. NAXISi, where i<=NAXISn'Length
+
+
+generic
+with procedure Plane_Data(Plane : out Tm_Arr; PlaneCount : in Positive_Count);
+procedure Write_Data_Unit_By_Planes
+    (F : in SIO.File_Type;
+    NAXISi : in NAXIS_Arr); -- Tm_Arr has size NAXIS1 .. NAXISi, where i<=NAXISn'Length
+
+
+end V3_Ints_Image;
+
--- lib/src/image/image_data.ads
+++ lib/src/image/image_data.ads
@@ -8,7 +8,7 @@ with Linear_Conv; use Linear_Conv;
 
 package Image_Data is
 
-
+ -- FF FI FU
 
 generic
 type Tmem is digits <>;
@@ -66,6 +66,67 @@ package FU is
     package Physical_Out is new Physical.Output(Linear4W);
 
 end FU;
+
+
+
+ -- rF II IU
+
+generic
+type Tmem is range <>;
+type Tmem_Arr is array (Positive_Count range <>) of Tmem;
+type Tcalc is digits <>;
+type Tfile  is digits <>;
+A,B: in out Tcalc;
+TmemNaN : Tmem;
+TfileNaN : Tfile;
+package rF is
+
+    package Physical is new Physical_Private(Tmem, Tmem_Arr, Tcalc, Tfile);
+    function Linear4R is new Linear_Conv.rF(Tfile, Tcalc, Tmem, A, B);
+    function Linear4W is new Linear_Conv.FI(Tmem, Tcalc, Tfile, A, B);
+
+    package Physical_In  is new Physical.Input(Linear4R);
+    package Physical_Out is new Physical.Output(Linear4W);
+
+end rF;
+
+
+
+generic
+type Tmem is range <>;
+type Tmem_Arr is array (Positive_Count range <>) of Tmem;
+type Tcalc is digits <>;
+type Tfile is range <>;
+A,B : in out Tcalc;
+package II is
+
+    package Physical is new Physical_Private(Tmem, Tmem_Arr, Tcalc, Tfile);
+    function Linear4R  is new Linear_Conv.II(Tfile, Tcalc, Tmem, A,B);
+    function Linear4W  is new Linear_Conv.II(Tmem, Tcalc, Tfile, A,B);
+
+    package Physical_In  is new Physical.Input(Linear4R);
+    package Physical_Out is new Physical.Output(Linear4W);
+
+end II;
+
+
+
+generic
+type Tmem is range <>;
+type Tmem_Arr is array (Positive_Count range <>) of Tmem;
+type Tcalc is digits <>;
+type Tfile is mod <>;
+A,B : in out Tcalc;
+package IU is
+
+    package Physical is new Physical_Private(Tmem, Tmem_Arr, Tcalc, Tfile);
+    function Linear4R  is new Linear_Conv.IU(Tfile, Tcalc, Tmem, A,B);
+    function Linear4W  is new Linear_Conv.UI(Tmem, Tcalc, Tfile, A,B);
+
+    package Physical_In  is new Physical.Input(Linear4R);
+    package Physical_Out is new Physical.Output(Linear4W);
+
+end IU;
 
 
 
