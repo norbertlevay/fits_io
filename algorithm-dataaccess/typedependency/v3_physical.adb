51874c81ea5d6b61c412a59f01765e4235cce8f3
Robert Butora <robert.butora@inaf.it>
2020-05-27 10:00:37 +0200

    alt_4/V3_Physical: cosmetic (comments only)

--- algorithm-dataaccess/typedependency/v3_physical.adb
+++ algorithm-dataaccess/typedependency/v3_physical.adb
@@ -2,9 +2,6 @@
 with Ada.Text_IO;
 
 
---generic
---type Tm is private;
---with F32_Physical is new Physical(Tm, Float_32);
 package body V3_Physical is
 
 package TIO renames Ada.Text_IO;
--- algorithm-dataaccess/typedependency/v3_physical.ads
+++ algorithm-dataaccess/typedependency/v3_physical.ads
@@ -8,7 +8,6 @@ generic
 type Tm is private;
 with package F32_Physical is new Physical(<>);
 with package I16_Physical is new Physical(<>);
---with package F32_Physical is new Physical(Tm, Float_32);
 package V3_Physical is
 
 
