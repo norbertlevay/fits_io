3e0ec645d7bcccd5a97f0b563b30563122407e0d
Robert Butora <robert.butora@inaf.it>
2020-07-19 13:17:00 +0200

    examples: tests F->I conversion Bugfix: adds Linear implementations for missing type combinations

--- examples/v3_pool_linear.ads
+++ examples/v3_pool_linear.ads
@@ -25,7 +25,22 @@ function Linear is new Linear_Pure (Integer_16, Float_32, Integer_16);
 function Linear is new Linear_Pure (Unsigned_8, Float_32, Unsigned_8);
 
 -- only for test F->UI : we need BLANK for output if NaN encoutered
+function Linear is new Linear_Pure           (Float_64, Float_64, Float_32);
 function Linear is new Linear_Check_UndefOut (Float_64, Float_64, Integer_64, Integer_64'Last);
+function Linear is new Linear_Check_UndefOut (Float_64, Float_64, Integer_32, Integer_32'Last);
+function Linear is new Linear_Check_UndefOut (Float_64, Float_64, Integer_16, Integer_16'Last);
+function Linear is new Linear_Check_UndefOut (Float_64, Float_64, Unsigned_8, Unsigned_8'Last);
+
+function Linear is new Linear_Check_UndefOut (Float_32, Float_32, Integer_64, Integer_64'Last);
+function Linear is new Linear_Check_UndefOut (Float_32, Float_32, Integer_32, Integer_32'Last);
+function Linear is new Linear_Check_UndefOut (Float_32, Float_32, Integer_16, Integer_16'Last);
+function Linear is new Linear_Check_UndefOut (Float_32, Float_32, Unsigned_8, Unsigned_8'Last);
+
+function Linear is new Linear_Check_UndefOut(Float_32,   Float_64, Integer_64, Integer_64'Last);
+function Linear is new Linear_Pure(Integer_32, Float_64, Integer_64);
+function Linear is new Linear_Pure(Integer_16, Float_64, Integer_64);
+function Linear is new Linear_Pure(Unsigned_8, Float_64, Integer_64);
+
 
 end V3_Pool_Linear;
 
