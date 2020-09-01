cdd4c2ceefa4e0ad89fac03b386141cc5e8d49dc
Robert Butora <robert.butora@inaf.it>
2020-09-01 22:35:49 +0300

    image: adds comments only on how should Physical_Write operate

--- lib/src/image/physical_write.ads
+++ lib/src/image/physical_write.ads
@@ -1,4 +1,18 @@
 
+-- NOTE: core must work in scope of all Ada-types (incl Interfaces)
+-- NOTE: it is specialization like V3 which provides some type-subset (and hides Tf)
+
+-- NOTE
+-- caller has data Tm and can choose Tf (even if looses significant precision)
+-- Write call will do necessary conversions and produce cards BZERO BSCALE:
+-- Tm = Float_N -> Tf = Float_M
+-- Tm = Float_N -> Tf = Int_M       yields: BZERO,BSCALE
+-- Tm = Float_N -> Tf = UInt_M      yields: BZERO,BSCALE
+-- Tm = Int_N  <-> Tf = Float_M     yields: BZERO,BSCALE
+-- Tm = UInt_N <-> Tf = Float_M     yields: BZERO,BSCALE
+-- Tm = Int_N  <-> Tf = UInt_M      yields: BZERO,BSCALE Tab.11
+
+
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 with Mandatory; use Mandatory; -- NAXIS_Arr needed
 with Optional; -- Card_Arr needed
