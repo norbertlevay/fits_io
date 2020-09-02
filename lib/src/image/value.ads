eafdec6b4769cf651ab2b66cec988e4069f7b410
Robert Butora <robert.butora@inaf.it>
2020-09-02 21:20:41 +0300

    image: cosmetic - fixes comments in Value spec

--- lib/src/image/value.ads
+++ lib/src/image/value.ads
@@ -1,36 +1,36 @@
 
 
 -- implements  Vout = A + B * Vin
+-- for all type combinations
 
 
 generic
-type Tout is private;   -- type in memory
-type Tc is digits <>; -- type in which scaling is calculated
-type Tin is private;   -- type in fits-file
+type Tout is private;   -- type of scaled output
+type Tc   is digits <>; -- type in which scaling is calculated
+type Tin  is private;   -- type of input
 
-with function Init_UOut(UInValid : in Boolean; UIn : in Tin; UOutValid : in out Boolean; UOut : in out Tout) return Boolean is <>;
+with function Init_UOut(UInValid :  in     Boolean; UIn  : in     Tin;
+                        UOutValid : in out Boolean; UOut : in out Tout) return Boolean is <>;
 
-with function Is_Undef(V,U : Tin; UValid : Boolean) return Boolean is <>;
+with function Is_Undef(V,U : Tin;  UValid : Boolean) return Boolean is <>;
 with function Is_Undef(V,U : Tout; UValid : Boolean) return Boolean is <>;
 
-with function "+"(R : Tin) return Tc is <>;
-with function "+"(R : Tc) return Tout is <>;
+with function "+"(R : Tin) return Tc   is <>;
+with function "+"(R : Tc ) return Tout is <>;
 
 
 package Value is
 
- UIn  : Tin;-- = BLANK$
- UInValid  : Boolean := False;
+ UIn      : Tin;
+ UInValid : Boolean := False;
 
-    A,B : Tc;
+ A,B : Tc;
 
---procedure Init(BZERO, BSCALE, BLANK : in String);
 
 
 procedure Init_Undef
-    (UInValid : in Boolean; UIn : in Tin;
+    (UInValid : in     Boolean; UIn  : in     Tin;
     UOutValid : in out Boolean; UOut : in out Tout);
-
 
 
 function Scaling(Vin : Tin) return Tout;
