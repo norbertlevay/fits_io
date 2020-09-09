a6d9fecfc27896320fd830185c71ee4bb9fe3733
Robert Butora <robert.butora@inaf.it>
2020-09-09 15:41:54 +0300

    add-on to previous commit: forgot to add these files

---
+++ examples/tt_app-minmax.adb
@@ -0,0 +1,48 @@
+
+with Ada.Text_IO;
+
+
+package body TT_App.Minmax is
+
+    package TIO renames Ada.Text_IO;
+
+    Min : Tm := T_Last;
+    Max : Tm := T_First;
+
+
+    procedure Plane_Data(E : Tm)
+    is
+    begin
+        if(not T_Valid(E))
+        then
+            Special_Count := Special_Count + 1; -- Invalid but not NaN
+        else
+            if(E > Max) then Max := E; end if;
+            if(E < Min) then Min := E; end if;
+        end if;
+    end Plane_Data;
+
+
+    procedure Undef_Data(E : Tm)
+    is
+    begin
+        Undef_Count := Undef_Count + 1;
+    end Undef_Data;
+
+
+    procedure Put_Results(UndefValid : in Boolean; UndefValue : in String)
+    is
+    begin
+        TIO.Put_Line("UndefValid             : " & Boolean'Image(UndefValid));
+        if(UndefValid)
+        then
+            TIO.Put_Line("UndefValue             : " & UndefValue);
+        end if;
+        TIO.Put_Line("Special_Count (Inf...) : " & SIO.Count'Image(Special_Count));
+        TIO.Put_Line("Undef_Count (NaN)      : " & SIO.Count'Image(Undef_Count));
+        TIO.Put_Line("Min                    : " & T_Image(Min));
+        TIO.Put_Line("Max                    : " & T_Image(Max));
+    end Put_Results;
+
+end TT_App.Minmax;
+
---
+++ examples/tt_app-minmax.ads
@@ -0,0 +1,26 @@
+
+with Ada.Streams.Stream_IO;
+
+with Physical.Data_Unit;
+
+  generic
+    package TT_App.Minmax is
+
+    package SIO renames Ada.Streams.Stream_IO;
+    use type SIO.Count;
+    
+    Special_Count : SIO.Count := 0; -- Inf...
+    Undef_Count   : SIO.Count := 0; -- NaN
+
+    package  T_Physical is new Physical(Tm,Tm_Arr,Tc, Tf);
+    package  T_Physical_DU is new T_Physical.Data_Unit;
+
+    procedure Plane_Data(E : Tm);
+    procedure Undef_Data(E : Tm);
+    procedure Read_Data_Unit is new T_Physical_DU.Read_Data_Unit(Plane_Data,Undef_Data);
+
+    procedure Put_Results(UndefValid : in Boolean; UndefValue : in String);
+
+  end TT_App.Minmax;
+
+
