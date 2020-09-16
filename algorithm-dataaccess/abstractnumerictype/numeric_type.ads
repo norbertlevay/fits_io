7de15e953c073c866e96f4a067d5bb66722d1652
Robert Butora <robert.butora@inaf.it>
2020-09-16 23:06:07 +0300

    algorithms: added comments to Numeric_Type pack

--- algorithm-dataaccess/abstractnumerictype/numeric_type.ads
+++ algorithm-dataaccess/abstractnumerictype/numeric_type.ads
@@ -6,5 +6,30 @@ package Numeric_Type is
     type Numeric is new T;
     type T_Arr is array (Positive range <>) of T;
     function Bit_Count return Positive;
+
+-- TODO
+    -- function To_Numeric(V : in Integer) return Numeric;
+    -- function To_Numeric(V : in Float) return Numeric;
+    -- ...
+    -- for all Ada-types see package Standard: (GNAT implementation)
+    -- https://en.wikibooks.org/wiki/Ada_Programming/Libraries/Standard/GNAT
+    -- function To_Integer(V : in Numeric) return Integer;
+    -- ...
+
+    -- GNAT/Standard basic num-types are:
+
+    -- integers are always 'range' metytype, not 'mod' !!
+
+    -- Short_Short_Integer
+    -- Short_Integer
+    -- Integer
+    -- Long_Integer
+    -- Long_Long_Integer
+
+    -- Short_Float
+    -- Float
+    -- Long_Float
+    -- Long_Long_Float
+
 end Numeric_Type;
 
