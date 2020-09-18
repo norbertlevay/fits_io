7a281810b8fc310fc2008bf1dffa7dc4cc4cb0e6
Robert Butora <robert.butora@inaf.it>
2020-09-18 17:36:50 +0300

    numeric: updates examples/convert to use Numeric_Type

--- examples/pool_for_numeric_type.adb
+++ examples/pool_for_numeric_type.adb
@@ -1,4 +1,7 @@
 
+with Ada.Text_IO;
+use Ada.Text_IO;
+
 
 package body Pool_For_Numeric_Type
 is
@@ -7,12 +10,14 @@ function "+"(V : in Float) return Long_Long_Float is begin return Long_Long_Floa
 function "+"(V : in Float) return Long_Float is begin return Long_Float(V); end "+";
 function "+"(V : in Float) return Float      is begin return Float(V);      end "+";
 function "+"(V : in Float) return Integer    is begin return Integer(V);    end "+";
+function "+"(V : in Float) return Short_Integer    is begin return Short_Integer(V);    end "+";
 
+function "+"(V : in Short_Integer)           return Float is begin return Float(V); end "+";
 function "+"(V : in Integer)           return Float is begin return Float(V); end "+";
---function "+"(V : in Float)             return Float is begin return Float(V); end "+";
 function "+"(V : in Long_Float)        return Float is begin return Float(V); end "+";
 function "+"(V : in Long_Long_Float)   return Float is begin return Float(V); end "+";
 
+function Is_Undef(V,U : in Short_Integer)    return Boolean is begin return (V = U); end Is_Undef;
 function Is_Undef(V,U : in Integer)    return Boolean is begin return (V = U); end Is_Undef;
 function Is_Undef(V,U : in Float)      return Boolean is begin return (not (V = V)); end Is_Undef;
 function Is_Undef(V,U : in Long_Float) return Boolean is begin return (not (V = V)); end Is_Undef;
@@ -23,6 +28,7 @@ function To_BITPIX(V : in Float  ) return Integer is begin return -V'Size; end T
 function To_BITPIX(V : in Long_Float) return Integer is begin return -V'Size; end To_BITPIX;
 function To_BITPIX(V : in Long_Long_Float) return Integer is begin return -V'Size; end To_BITPIX;
 function To_BITPIX(V : in Integer) return Integer is begin return  V'Size; end To_BITPIX;
+function To_BITPIX(V : in Short_Integer) return Integer is begin return  V'Size; end To_BITPIX;
 
 
 
--- examples/pool_for_numeric_type.ads
+++ examples/pool_for_numeric_type.ads
@@ -7,11 +7,14 @@ function "+"(V : in Float) return Long_Long_Float;
 function "+"(V : in Float) return Long_Float;
 function "+"(V : in Float) return Float;
 function "+"(V : in Float) return Integer;
+function "+"(V : in Float) return Short_Integer;
 
+function "+"(V : in Short_Integer)           return Float;
 function "+"(V : in Integer)           return Float;
 function "+"(V : in Long_Float)        return Float;
 function "+"(V : in Long_Long_Float)   return Float;
 
+function Is_Undef(V,U : in Short_Integer)    return Boolean;
 function Is_Undef(V,U : in Integer)    return Boolean;
 function Is_Undef(V,U : in Float)      return Boolean;
 function Is_Undef(V,U : in Long_Float) return Boolean;
@@ -22,6 +25,7 @@ function To_BITPIX(V : in Float  ) return Integer;
 function To_BITPIX(V : in Long_Float) return Integer;
 function To_BITPIX(V : in Long_Long_Float) return Integer;
 function To_BITPIX(V : in Integer) return Integer;
+function To_BITPIX(V : in Short_Integer) return Integer;
 
 
 
