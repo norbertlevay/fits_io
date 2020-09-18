54c74b02a966a0fe2cff7430bf4572fe87673e52
Robert Butora <robert.butora@inaf.it>
2020-09-18 16:00:24 +0300

    numeric: adds Read/Write_Data_Unit with call-back by elements

--- lib/src/image/scaling-streams.adb
+++ lib/src/image/scaling-streams.adb
@@ -1,6 +1,6 @@
 
 with Endian;
-
+with File.Misc; -- Write_Padding needed
 
 package body Scaling.Streams is
 
@@ -29,6 +29,68 @@ begin
     CheckAndRevert(Aout);
     Tdst.Numeric_Arr'Write(Sdst, Aout);
 end Linear;
+
+
+
+-- Data Unit access
+
+
+
+function DU_Data_Count(NAXISn : in NAXIS_Arr) return Positive_Count
+is
+  Acc : Positive_Count := 1;
+begin
+  for I in NAXISn'Range
+  loop
+    Acc := Acc * NAXISn(I);
+  end loop;
+  return Acc;
+end DU_Data_Count;
+
+
+-- FIXME both miss Undefed handling and scaling (set A,B)
+
+-- src : file
+-- dst : array in memory
+procedure Read_Data_Unit
+  (F : SIO.File_Type;
+  NAXISn : in NAXIS_Arr)
+is
+    DULength : Positive_Count := DU_Data_Count(NAXISn);
+    E : Tsrc.Numeric;
+begin
+  for I in 1 .. DULength
+  loop
+    Tsrc.Numeric'Read(SIO.Stream(F), E);
+    Elem(Linear(E));
+  end loop;
+end Read_Data_Unit;
+
+
+
+-- src : array in memory
+-- dst : file
+procedure Write_Data_Unit
+  (F : SIO.File_Type;
+  NAXISn : in NAXIS_Arr)
+is
+    DULength : Positive_Count := DU_Data_Count(NAXISn);
+    E : Tsrc.Numeric;
+begin
+  for I in 1 .. DULength
+  loop
+    Elem(E);
+    Tdst.Numeric'Write(SIO.Stream(F), Linear(E));
+  end loop;
+
+  File.Misc.Write_Padding(F,SIO.Index(F),File.Misc.HeaderPadValue);
+
+end Write_Data_Unit;
+
+
+
+
+
 
 
 
--- lib/src/image/scaling-streams.ads
+++ lib/src/image/scaling-streams.ads
@@ -1,5 +1,7 @@
 
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
+with Mandatory; use Mandatory; -- NAXIS_Arr needed
+
 
 generic
 package Scaling.Streams is
@@ -8,9 +10,29 @@ package SIO renames Ada.Streams.Stream_IO;
 
 -- Read: replaces source-array with Stream
 procedure Linear(Ssrc : SIO.Stream_Access; Aout : out Tdst.Numeric_Arr);
-
 -- Write: replaces destination-array with Stream
 procedure Linear(Ain : in Tsrc.Numeric_Arr; Sdst : SIO.Stream_Access);
+
+
+-- Data Unit access
+
+-- src File --> Tdst
+generic
+  with procedure Elem(E : in Tdst.Numeric);
+procedure Read_Data_Unit
+  (F : SIO.File_Type;
+  NAXISn : in NAXIS_Arr);
+
+-- Tsrc --> dst is File
+generic
+  with procedure Elem(E : out Tsrc.Numeric);
+procedure Write_Data_Unit
+  (F : SIO.File_Type;
+  NAXISn : in NAXIS_Arr);
+
+
+
+
 
 end Scaling.Streams;
 
