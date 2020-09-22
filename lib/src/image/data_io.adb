d62a159c1f40a900d14f69a8aa447d1fa77371d5
Robert Butora <robert.butora@inaf.it>
2020-09-22 14:29:48 +0300

    numeric: renames in data_io packages Tf->Raw Tm->Physical and some clean-up

--- lib/src/image/data_io.adb
+++ lib/src/image/data_io.adb
@@ -1,65 +1,47 @@
 
 with Ada.Streams.Stream_IO;
 with Numeric_Type;
-with Scaling;
-with Image;
 with Endian;
 
---generic
--- with package Tf is new Numeric_Type(<>);
--- with package Tm is new Numeric_Type(<>);
--- with package Tm_Image is new Image(<>);--T => Tm; others => <>); 
+-- FIXME stack/local arrays: Af Fin_Arr Fout_Arr Af, use 3x more space: price to pay
+-- for saving if(Use_Undef AND Is_Undef()) at cycle twice
+
 
 package body Data_IO is
 
-Uf : Tf.Numeric;
-Um : Tm.Numeric; -- FIXME use Raw and Physical instead Tf and Tm
-Use_Undefs : Boolean := False;
-
--- FIXME set undef values from Tm_Image.DM - we miss calc the "other" side Undef
-procedure Set_Undefined(Ufile : in Tf.Numeric; Umem : in Tm.Numeric)
-is
-begin
-    Uf := Ufile;
-    Um := Umem;
-    Use_Undefs := True;
-end Set_Undefined;
-
-
-
-
-
+package Phys renames Physical;
 
 procedure Read_Array
     (F : SIO.File_Type;
-    A,B     : in Float;
-    Tm_Arr  : out Tm.Numeric_Arr)
+    A,B : in Float;
+    Phys_Arr : out Physical.Numeric_Arr)
 is
- Af : Tf.Numeric_Arr(Tm_Arr'Range);
- Fin_Arr  : Tf.Float_Arr(Tm_Arr'Range); -- FIXME uses 3x more space (Af Fin Fout arrays): price
- Fout_Arr : Tm.Float_Arr(Tm_Arr'Range); -- for saving if(Use_Undef AND Is_Undef()) at cycle twice
- procedure CheckAndRevert is new Endian.Check_And_Revert(Tf.Numeric,Tf.Numeric_Arr);
+ Af         : Raw.Numeric_Arr(Phys_Arr'Range);
+ Fin_Arr    : Raw.Float_Arr(Phys_Arr'Range);
+ Fout_Arr   : Phys.Float_Arr(Phys_Arr'Range);
+ procedure CheckAndRevert is new Endian.Check_And_Revert(Raw.Numeric,Raw.Numeric_Arr);
 begin
 
-    Tf.Numeric_Arr'Read(SIO.Stream(F), Af);
+    -- low-level read
+
+    Raw.Numeric_Arr'Read(SIO.Stream(F), Af);
     CheckAndRevert(Af);
 
-    --    TfTm_Scaling(Af,Tm_Arr,A,B,Uf,Um);
+    -- conversions
 
-    Fin_Arr := Tf.To_Float(Af);
+    Fin_Arr := Raw.To_Float(Af);
     for I in Fin_Arr'Range
     loop
         Fout_Arr(I) := A + B * Fin_Arr(I);
     end loop;
 
     -- calc target-domain's Undef Value
-
-    if(Tf.Is_Undefined_Valid)
+    if(Raw.Is_Undefined_Valid)
     then
-        Tm.Set_Undefined(Tm."+"(A + B * Tf."+"(Tf.Get_Undefined)));
+        Phys.Set_Undefined(Phys."+"(A + B * Raw."+"(Raw.Get_Undefined)));
     end if;
 
-    Tm_Arr := Tm.To_Numeric(Fout_Arr);
+    Phys_Arr := Phys.To_Numeric(Fout_Arr);
 
 end Read_Array;
 
@@ -67,35 +49,35 @@ end Read_Array;
 
 procedure Write_Array
     (F : SIO.File_Type;
-    A,B     : in Float;
-    Tm_Arr  : in Tm.Numeric_Arr)
+    A,B : in Float;
+    Phys_Arr : in Physical.Numeric_Arr)
 is
- Af         : Tf.Numeric_Arr(Tm_Arr'Range);
- Fin_Arr    : Tm.Float_Arr(Tm_Arr'Range);
- Fout_Arr   : Tf.Float_Arr(Tm_Arr'Range);
-procedure CheckAndRevert is new Endian.Check_And_Revert(Tf.Numeric,Tf.Numeric_Arr);
+ Af         : Raw.Numeric_Arr(Phys_Arr'Range);
+ Fin_Arr    : Phys.Float_Arr(Phys_Arr'Range);
+ Fout_Arr   : Raw.Float_Arr(Phys_Arr'Range);
+procedure CheckAndRevert is new Endian.Check_And_Revert(Raw.Numeric,Raw.Numeric_Arr);
 begin
 
-    --    TmTf_Scaling(Tm_Arr,Af,A,B,Uf,Um);
+    -- conversions
 
-    Fin_Arr := Tm.To_Float(Tm_Arr);
+    Fin_Arr := Phys.To_Float(Phys_Arr);
     for I in Fin_Arr'Range
     loop
         Fout_Arr(I) := A + B * Fin_Arr(I);
     end loop;
 
     -- calc target-domain's Undef Value
-
-    if(Tm.Is_Undefined_Valid)
+    if(Phys.Is_Undefined_Valid)
     then
-        Tf.Set_Undefined(Tf."+"(A + B * Tm."+"(Tm.Get_Undefined)));
+        Raw.Set_Undefined(Raw."+"(A + B * Phys."+"(Phys.Get_Undefined)));
     end if;
 
-    Af := Tf.To_Numeric(Fout_Arr);
+    Af := Raw.To_Numeric(Fout_Arr);
 
     -- low-level write
+
     CheckAndRevert(Af);
-    Tf.Numeric_Arr'Write(SIO.Stream(F), Af);
+    Raw.Numeric_Arr'Write(SIO.Stream(F), Af);
 
 end Write_Array;
 
--- lib/src/image/data_io.ads
+++ lib/src/image/data_io.ads
@@ -1,25 +1,23 @@
 
 with Ada.Streams.Stream_IO;
 with Numeric_Type;
-with Scaling;
-with Image;
 
 generic
- with package Tf is new Numeric_Type(<>);
- with package Tm is new Numeric_Type(<>);
+ with package Raw       is new Numeric_Type(<>);
+ with package Physical  is new Numeric_Type(<>);
 package Data_IO is
 
     package SIO renames Ada.Streams.Stream_IO;
 
 procedure Read_Array
     (F : SIO.File_Type;
-    A,B     : in Float;
-    Tm_Arr  : out Tm.Numeric_Arr);
+    A,B : in Float;
+    Phys_Arr : out Physical.Numeric_Arr);
 
 procedure Write_Array
     (F : SIO.File_Type;
-    A,B     : in Float;
-    Tm_Arr  : in Tm.Numeric_Arr);
+    A,B : in Float;
+    Phys_Arr : in Physical.Numeric_Arr);
 
 end Data_IO;
 
