5ff5aa28a94a0003e12477c8dff86fdbd5044c6a
Robert Butora <robert.butora@inaf.it>
2020-09-23 13:25:34 +0300

    numeric: empty Read Write in FITS_IO

--- examples/fits_io.adb
+++ examples/fits_io.adb
@@ -1,61 +1,38 @@
 
-with Ada.Text_IO;
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 
 with Numeric_Type;
 with Array_IO;
 
-with Pool_For_Numeric_Type; use Pool_For_Numeric_Type;
+--generic
+-- type T is private;
+-- type T_Arr is array (Positive_Count range <>) of T;
+
+-- from Numeric_Type pool
+ --with function "+"(V : in T)     return Float is <>;
+--- with function "+"(V : in Float) return T is <>;
+
+-- with function Is_Undef(V,U : in T) return Boolean is <>;
+-- with function To_BITPIX(V : in T) return Integer is <>;
 
 package body FITS_IO is
-
---    type T_Arr is array(SIO.Positive_Count range <>) of T;
-    type Float_Arr is array(SIO.Positive_Count range <>) of Float;
 
 
 procedure Read
     (F : SIO.File_Type;
     TArr : out T_Arr)
 is
-
-    package Phys is new Numeric_Type(T,T_Arr,Float_Arr);
-    package Raw  is new Numeric_Type(Float,Float_Arr,Float_Arr);
-    -- FIXME type is determined by BITPIX after having read the Header
-    -- now assume Float...
-    package AIO  is new Array_IO(Raw,Phys);
-
-    A,B : Float;
-    Ph_Arr : Phys.Numeric_Arr(TArr'Range);
 begin
-    -- assume Header was read, we have A,B and Undef
-    -- if BLANK in Header -> set Undef into Raw
-    A := 0.0;
-    B := 10.0;
-    AIO.Read(F, A,B, Ph_Arr);
-
-    for I in Ph_Arr'Range
-    loop
-        Ada.Text_IO.Put_Line("DBG " & Float'Image(Phys.To_Float(Ph_Arr(I))));
-        TArr(I) := Ph_Arr(I);
-    end loop;
-
+    null;
 end Read;
-
 
 procedure Write
     (F : SIO.File_Type;
     TArr : in T_Arr)
 is
-    A,B : Float;
-
-    package Phys is new Numeric_Type(T,T_Arr,Float_Arr);
-    package Raw  is new Numeric_Type(T,T_Arr,Float_Arr);
-    package AIO  is new Array_IO(Raw,Phys);
-
 begin
-    -- assume Header was read, we have A,B and Undef
-    null;
+ null;
 end Write;
-
 
 
 end FITS_IO;
--- examples/fits_io.ads
+++ examples/fits_io.ads
@@ -1,19 +1,23 @@
 
+
+-- FITS file format (may) store data in different
+-- format/type as the one in which the data is provided
+-- and also allows to reserve one value to mark undefined
+-- values in the array
+
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 
 with Numeric_Type;
 with Array_IO;
 
+
 generic
  type T is private;
  type T_Arr is array (Positive_Count range <>) of T;
 
--- from Numeric_Type pool
- with function "+"(V : in T)     return Float is <>;
+ with function "+"(V : in T) return Float is <>;
  with function "+"(V : in Float) return T is <>;
-
  with function Is_Undef(V,U : in T) return Boolean is <>;
- with function To_BITPIX(V : in T) return Integer is <>;
 
 package FITS_IO is
 
