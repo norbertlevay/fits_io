5c986edcaa6d7527036934f739414ad0addd42a3
Robert Butora <robert.butora@inaf.it>
2020-10-07 22:43:15 +0300

    numeric: in Buffer_Type replace generic type-T param with package-Phys generic param

--- lib/src/image/buffer_type.adb
+++ lib/src/image/buffer_type.adb
@@ -1,43 +1,30 @@
 
+with Ada.Text_IO; use Ada.Text_IO;
+
 with Numeric_Type;
+with Pool_For_Numeric_Type; use Pool_For_Numeric_Type;
 with Array_IO;
 
-with Ada.Text_IO;
-use Ada.Text_IO;
+
 
 package body Buffer_Type is
 
  type Float_Arr is array (SIO.Positive_Count range <>) of Float;
- type T_Arr is array (SIO.Positive_Count range <>) of T;
- -- FIXME replace Short_Integer with Integer_16
- -- use here only values allowed inside FITS files
- type I16_Arr is array (SIO.Positive_Count range <>) of Short_Integer;
+ type I16_Arr   is array (SIO.Positive_Count range <>) of Short_Integer;
 
- function "+"(V : in Float) return Short_Integer is begin return Short_Integer(V); end "+";
- function "+"(V : in Short_Integer)     return Float is begin return Float(V); end "+";
+ package I16Raw  is new Numeric_Type(Short_Integer, I16_Arr,    Float_Arr);
+ package F32Raw  is new Numeric_Type(Float,         Float_Arr,  Float_Arr);
 
- function Is_Undef(V,U : in Short_Integer) return Boolean is begin return (V=U); end Is_Undef;
- function Is_Undef(V,U : in Float)         return Boolean is begin return Not (V=V); end Is_Undef;
- function To_BITPIX(V : in Short_Integer) return Integer is begin return V'Size; end To_BITPIX;
- function To_BITPIX(V : in Float)         return Integer is begin return -(V'Size); end To_BITPIX;
-
- -- prepare the conversion arrays
-
- package Phys is new Numeric_Type(T, Buffer, Float_Arr);
-
- package TRaw  is new Numeric_Type(T, T_Arr, Float_Arr);
- package T_AIO is new Array_IO(TRaw,Phys);
-
- package I16Raw  is new Numeric_Type(Short_Integer, I16_Arr, Float_Arr);
- package I16_AIO is new Array_IO(I16Raw,Phys);
-
- package F32Raw  is new Numeric_Type(Float, Float_Arr, Float_Arr);
- package F32_AIO is new Array_IO(F32Raw,Phys);
+ package I16_AIO is new Array_IO(I16Raw, Phys);
+ package F32_AIO is new Array_IO(F32Raw, Phys);
 
 
 
 
-procedure Read_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : out Buffer)
+
+procedure Read_Buffer
+    (S: not null access Ada.Streams.Root_Stream_Type'Class;
+    Item : out Buffer)
 is
 begin
 
@@ -62,7 +49,6 @@ begin
 
 
     case(File_BITPIX) is
-        when   0 =>   T_AIO.Read(S, A,B, Item);
         when  16 => I16_AIO.Read(S, A,B, Item);
         when -32 => F32_AIO.Read(S, A,B, Item);
         when  8 | 32 | 64 | -64 => null; -- Warn: "Not implemented"
@@ -72,7 +58,11 @@ end Read_Buffer;
 
 
 
-procedure Write_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : in Buffer)
+
+
+procedure Write_Buffer
+    (S: not null access Ada.Streams.Root_Stream_Type'Class;
+    Item : in Buffer)
 is
 begin
 
@@ -84,7 +74,6 @@ begin
         if(File_Undefined_Valid)
         then
             case(File_BITPIX) is
-                when   0 =>   TRaw.Set_Undefined(  TRaw.To_Numeric(+File_Undefined_Value));
                 when  16 => I16Raw.Set_Undefined(I16Raw.To_Numeric(+File_Undefined_Value));
                 when -32 => F32Raw.Set_Undefined(F32Raw.To_Numeric(+File_Undefined_Value));
                 when  8 | 32 | 64 | -64 => null; -- Warn: "Not implemented"
@@ -96,7 +85,6 @@ begin
 
 
     case(File_BITPIX) is
-        when   0 =>   T_AIO.Write(S, A,B, Item);
         when  16 => I16_AIO.Write(S, A,B, Item);
         when -32 => F32_AIO.Write(S, A,B, Item);
         when  8 | 32 | 64 | -64 => null; -- Warn: "Not implemented"
--- lib/src/image/buffer_type.ads
+++ lib/src/image/buffer_type.ads
@@ -1,4 +1,3 @@
-with Ada.Streams.Stream_IO;use Ada.Streams.Stream_IO;
 
 -- FITS allows/envcourages to store the data in different type as it is available:
 -- [A,B, Target_BITPIX]-triple determines the type in which data is stored and proper scaling
@@ -8,44 +7,48 @@ with Ada.Streams.Stream_IO;use Ada.Streams.Stream_IO;
 --  BSCALE = f(A,B)  BZERO=f(A,B) 
 --  So Vout = A + B * Vin used in Write
 --  is the inverse func to: Vout = BZERO + BSCALE * Vin
-
+--
 -- Target_BITPIX := 0 -> means store in the same type as T
 -- FIXME what to use for Target_Type/DU_Type/File_Type/Target_BITPIX ? 
 -- enum or ?
-
-
-generic
-type T is private;
-Memory_Undefined_Value  : T;
-Memory_Undefined_Valid  : Boolean := False; -- if valid
-File_Undefined_Value    : Float := 0.0;
-File_Undefined_Valid    : Boolean := False;
-A : in Float    := 0.0;
-B : in Float    := 1.0;
-Memory_BITPIX   : Integer := 0; -- zero means the same as T, no scaling needed A,B=(0,1)
-File_BITPIX     : Integer :=0;
-
-with function "+"(V : in Float) return T     is <>; 
-with function "+"(V : in T)     return Float is <>; 
-with function Is_Undef(V,U : in T) return Boolean is <>; 
-with function To_BITPIX(V : in T) return Integer is <>; 
-
-package Buffer_Type is
-
-    package SIO renames Ada.Streams.Stream_IO;
-
-type Buffer is array (Positive_Count range <>) of T;
-
-procedure Read_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : out Buffer);
-procedure Write_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : in Buffer);
-
-for Buffer'Write use Write_Buffer;
-for Buffer'Read use Read_Buffer;
+--
+-- NOTE on 'Read 'Write funcs
+--for Physical.Numeric_Arr'Write use Write_Buffer;
+--for Buffer'Read use Read_Buffer;
 -- normally 'Write 'Read would be overriden on type T not T_Arr
 -- but because we have Array_IO due to efficiency (dont ask Undef_Valid and Is_Undef in
 -- each cycle-round). 
 -- So overriden 'Write 'Read calling (Scaling and Conversion) would be attached to T not T_Arr
 
+
+with Ada.Streams.Stream_IO;use Ada.Streams.Stream_IO;
+with Numeric_Type;
+
+
+generic
+ with package Phys is new Numeric_Type(<>);
+
+ Memory_Undefined_Value  : Phys.Numeric;
+ Memory_Undefined_Valid  : Boolean := False; -- if valid
+ File_Undefined_Value    : Float := 0.0;
+ File_Undefined_Valid    : Boolean := False;
+ A : in Float    := 0.0;
+ B : in Float    := 1.0;
+ Memory_BITPIX   : Integer := 0; -- zero means the same as T, no scaling needed A,B=(0,1)
+ File_BITPIX     : Integer :=0;
+
+
+package Buffer_Type is
+
+    package SIO renames Ada.Streams.Stream_IO;
+
+    subtype Buffer is Phys.Numeric_Arr;
+
+procedure Read_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : out Buffer);
+procedure Write_Buffer(S: not null access Ada.Streams.Root_Stream_Type'Class; Item : in Buffer);
+
+--for Physical.Numeric_Arr'Write use Write_Buffer;
+--for Buffer'Read use Read_Buffer;
 
 end Buffer_Type;
 
