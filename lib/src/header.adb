0c7a2c93372da2840037156c0916ed5af1c86c64
Robert Butora <robert.butora@inaf.it>
2021-01-22 18:30:03 +0200

    alg: cosmetic (ident in header.adb)

--- lib/src/header.adb
+++ lib/src/header.adb
@@ -53,118 +53,114 @@ with Card;
 
 package body Header is
 
-    package TIO renames Ada.Text_IO;
---    package KW renames Keyword_Record;
+   package TIO renames Ada.Text_IO;
+
+   CardsCntInBlock : constant Positive := 36;
+   -- FIXME Card_Block is used in File.Misc::Copy_Blocks & Copy_HDU
+   -- find other solution for File.Misc, here move it inside body
+   type Card_Block is array (Positive range 1..CardsCntInBlock) of String_80;
+   pragma Pack (Card_Block);
+   -- FIXME does Pack guarantee arr is packed? how to guarantee Arrs are packed
+   -- OR do we need to guarantee at all ?
 
 
-    CardsCntInBlock : constant Positive := 36;
-    -- FIXME Card_Block is used in File.Misc::Copy_Blocks & Copy_HDU
-    -- find other solution for File.Misc, here move it inside body
-    type Card_Block is array (Positive range 1..CardsCntInBlock) of String_80;
-    pragma Pack (Card_Block);
-    -- FIXME does Pack guarantee arr is packed? how to guarantee Arrs are packed
-    -- OR do we need to guarantee at all ?
-
-
-
-
-   --
-   -- Read File until ENDCard found
-   --
+   -- buffered Read
 
    procedure Read_Card (File : in SIO.File_Type;
-            HStart : in Positive_Count;
-                        CurBlkNum : in out Count;
-                        Blk : in out Card_Block;
-                        CardNum : in Positive_Count;
-                        Card    : out  String)
+      HStart : in Positive_Count;
+      CurBlkNum : in out Count;
+      Blk : in out Card_Block;
+      CardNum : in Positive_Count;
+      Card    : out  String)
    is
-         BlockSize_SIOunits : constant Positive_Count := 2880;
-         BlkNum : Positive_Count;
-         CardNumInBlk : Natural;
-         BlkNumIndex : Positive_Count;
+      BlockSize_SIOunits   : constant Positive_Count := 2880;
+      BlkNum               : Positive_Count;
+      CardNumInBlk         : Natural;
+      BlkNumIndex          : Positive_Count;
    begin
-         BlkNum := 1 + (CardNum - 1) / 36; 
-         CardNumInBlk := Natural(CardNum - (BlkNum - 1) * 36);-- FIXME is < 36 Natural is ok if two lines together
+      BlkNum := 1 + (CardNum - 1) / 36; 
+      CardNumInBlk := Natural(CardNum - (BlkNum - 1) * 36);
+      -- FIXME is < 36 Natural is ok if two lines together
 
-         if(BlkNum /= CurBlkNUm)
-         then
-                        -- FIXME BEGIN only this section depends on SIO. file access
-                        -- make it Read_Block(SIO.File, FileBlkNum, Blk)
-                        -- where FileBlkNum := HStart + BlkNum
-                        -- BlkNum - relative to HDU start
-                        -- FileBlkNum - relative to File start
-               BlkNumIndex :=  HStart + (BlkNum-1) * BlockSize_SIOunits;
+      if(BlkNum /= CurBlkNUm)
+      then
+         -- FIXME BEGIN only this section depends on SIO. file access
+         -- make it Read_Block(SIO.File, FileBlkNum, Blk)
+         -- where FileBlkNum := HStart + BlkNum
+         -- BlkNum - relative to HDU start
+         -- FileBlkNum - relative to File start
+         BlkNumIndex :=  HStart + (BlkNum-1) * BlockSize_SIOunits;
 
-               SIO.Set_Index(File, SIO.Count(BlkNumIndex));
-               -- FIXME cast
-               Card_Block'Read(SIO.Stream(File), Blk);
-               CurBlkNum := BlkNum;
-               -- FIXME END   only this section depends on SIO. file access
-         end if;
+         SIO.Set_Index(File, SIO.Count(BlkNumIndex));
+         -- FIXME cast
+         Card_Block'Read(SIO.Stream(File), Blk);
+         CurBlkNum := BlkNum;
+         -- FIXME END   only this section depends on SIO. file access
+      end if;
 
-         Card := Blk(CardNumInBlk);
+      Card := Blk(CardNumInBlk);
 
    end Read_Card;
 
 
--- read info help in Mandatory keys of the Header
-  function  Read_Mandatory (FitsFile : in SIO.File_Type) return Mandatory.Result_Rec
-  is
-        HeaderStart : Positive_Count := Count(SIO.Index(FitsFile));--FIXME cast
-                CardNum : Count;
-                Card : String(1..80);
 
-                CurBlkNum : Count := 0; -- none read yet
-                Blk : Card_Block;
+   -- Parse Header Elements
+
+   function  Read_Mandatory (FitsFile : in SIO.File_Type) return Mandatory.Result_Rec
+   is
+      HeaderStart : Positive_Count := Count(SIO.Index(FitsFile));--FIXME cast
+      CardNum     : Count;
+      Card        : String(1..80);
+      CurBlkNum   : Count := 0; -- none read yet
+      Blk         : Card_Block;
    begin
-                CardNum := Mandatory.Reset_State;
-                loop
-                        Read_Card(FitsFile, HeaderStart, CurBlkNum, Blk, CardNum, Card);
-                        CardNum := Mandatory.Next(CardNum, Card);
-                        exit when (CardNum = 0); 
-                end loop;
 
-                -- calc HDU size
+      CardNum := Mandatory.Reset_State;
+      loop
+         Read_Card(FitsFile, HeaderStart, CurBlkNum, Blk, CardNum, Card);
+         CardNum := Mandatory.Next(CardNum, Card);
+         exit when (CardNum = 0); 
+      end loop;
 
-                declare
-                        PSize : Mandatory.Result_Rec := Mandatory.Get;
-                begin
-            return PSize;
-                end;
+      declare
+         PSize : Mandatory.Result_Rec := Mandatory.Get;
+      begin
+         return PSize;
+      end;
 
    end Read_Mandatory;
 
 
+
    function  Read_Optional (FitsFile : in  SIO.File_Type;
-                       Keys : in Optional.Bounded_String_8_Arr)
-      return Card_Arr
+      Keys : in Optional.Bounded_String_8_Arr)
+      return Optional.Card_Arr
    is
-    HeaderStart : Positive_Count := Positive_Count(SIO.Index(FitsFile));
-        CardNum : Count;
-        Card : String(1..80);
-
-    CurBlkNum : Count := 0; -- none read yet
-    Blk : Card_Block;
+      HeaderStart : Positive_Count := Positive_Count(SIO.Index(FitsFile));-- FIXME cast
+      CardNum     : Count;
+      Card        : String(1..80);
+      CurBlkNum   : Count := 0; -- none read yet
+      Blk         : Card_Block;
    begin
-        CardNum := Optional.Init(Keys);
-        loop
-        Read_Card(FitsFile, HeaderStart, CurBlkNum, Blk, CardNum, Card);
-                CardNum := Optional.Next(CardNum, Card);
-                exit when (CardNum = 0); 
-        end loop;
 
+      CardNum := Optional.Init(Keys);
+      loop
+         Read_Card(FitsFile, HeaderStart, CurBlkNum, Blk, CardNum, Card);
+         CardNum := Optional.Next(CardNum, Card);
+         exit when (CardNum = 0); 
+      end loop;
 
-    declare
-        Cards : Card_Arr := Optional.Get_Cards;
-    begin
-        return Cards;
-    end;
+      declare
+         Cards : Optional.Card_Arr := Optional.Get_Cards;
+      begin
+         return Cards;
+      end;
 
    end Read_Optional;
 
 
-   -- Compose
+
+   -- Compose Header Elements
 
    procedure Write_ENDCard_With_Padding(FFile : SIO.File_Type)
    is
@@ -172,6 +168,7 @@ package body Header is
       String_80'Write(SIO.Stream(FFile), ENDCard);
       File.Misc.Write_Padding(FFile, SIO.Index(FFile), File.Misc.HeaderPadValue);
    end Write_ENDCard_With_Padding;
+
 
 end Header;
 
--- lib/src/header.ads
+++ lib/src/header.ads
@@ -19,15 +19,13 @@
    -- * RULE ENDCard is last and follwed by padding
 
 with Ada.Streams.Stream_IO;
---with FITS_IO; use FITS_IO; -- NAXIS_Array needed
-with Mandatory;-- Result_Rec needed
---with Ada.Strings.Bounded; use Ada.Strings.Bounded; -- Max20 only FIXME !!
-with Optional; use Optional;-- Bounded_String_8_Arr & Card_Arr needed 
+
+with Mandatory; -- Result_Rec needed
+with Optional;  -- Bounded_String_8_Arr & Card_Arr needed 
 
 
 package Header is
 
-   -- package SIO renames FITS_IO;
    package SIO renames Ada.Streams.Stream_IO;
 
 
@@ -38,18 +36,12 @@ package Header is
 
    function  Read_Optional
       (FitsFile : in  SIO.File_Type;
-      Keys : in Optional.Bounded_String_8_Arr) return Card_Arr;
+      Keys : in Optional.Bounded_String_8_Arr) return Optional.Card_Arr;
 
 
    -- API constructing Header: use the Composer, supplying Card-groups
 
    procedure Write_ENDCard_With_Padding(FFile : SIO.File_Type);
- 
-   -- procedure Write_Cards(F : in SIO.File_Type; Cards : in Card_Arr);
-      -- adds cards after last written; call several times until header completed
-
-   -- procedure Close(F : in FITS_IO.File_Type);
-      -- writes last END-card and padding
 
 
 end Header;
