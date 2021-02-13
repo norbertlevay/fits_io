43963249f4f8d8d34bef75dbe2e83c539e6b8ecb
Robert Butora <robert.butora@inaf.it>
2021-02-14 00:07:33 +0200

    stream: cleans image/Value (no IO anymore - only conversion Raw-Phys)

--- lib/src/image/value.adb
+++ lib/src/image/value.adb
@@ -74,33 +74,6 @@ end Raw_To_Phys;
 
 
 
-procedure Read
-    (S : not null access Ada.Streams.Root_Stream_Type'Class;
-    A,B : in Float;
-    Phys_Arr : out Physical.Numeric_Arr)
-is
-   Af : Raw.Numeric_Arr(Phys_Arr'Range);
-   procedure Raw_CheckAndRevert is new Endian.Check_And_Revert(Raw.Numeric,Raw.Numeric_Arr);
-
-   Dummy : Raw.Numeric;
-   NBits : Integer := Af'Length * abs Raw.To_BITPIX(Dummy);
-   -- FIXME how to find Object_Size of Raw.Numeric_Arr which is generic here ?
-   -- GEM39: Item <-> Af and Buffer <-> Raw.Numeric_Arr
-   -- Item_Size : constant Stream_Element_Offset := Buffer'Object_Size / Stream_Element'Size;
-   Raw_Arr_Size : constant Stream_Element_Offset :=
-                                 Ada.Streams.Stream_Element_Offset(NBits) / Stream_Element'Size;
-   type SEA_Pointer is access all Stream_Element_Array (1 .. Raw_Arr_Size);
-   function As_SEA_Pointer is new Ada.Unchecked_Conversion (System.Address, SEA_Pointer);
-   Length : Ada.Streams.Stream_Element_Offset;
-begin
-   --Raw.Numeric_Arr'Read(S, Af);
-   Ada.Streams.Read(S.all, As_SEA_Pointer(Af'Address).all, Length );
-   --Ada.Streams.Stream_Element_Array'Read(S, As_SEA_Pointer(Af'Address).all );
-   Raw_CheckAndRevert(Af);
-   -- low level read done
-   --Raw_To_Phys(Af, A,B, Phys_Arr);
-end Read;
-
 
 
 procedure Phys_To_Raw
@@ -153,36 +126,6 @@ begin
    Raw_Arr := Af;
 
 end Phys_To_Raw;
-
-
-
-procedure Write
-    (S : not null access Ada.Streams.Root_Stream_Type'Class;
-    A,B : in Float;
-    Phys_Arr : in Physical.Numeric_Arr)
-is
-   Af : Raw.Numeric_Arr(Phys_Arr'Range);
-   procedure Raw_CheckAndRevert is new Endian.Check_And_Revert(Raw.Numeric,Raw.Numeric_Arr);
-
-   Dummy : Raw.Numeric;
-   NBits : Integer := Af'Length * abs Raw.To_BITPIX(Dummy);
-   -- FIXME how to find Object_Size of Raw.Numeric_Arr which is generic here ?
-   -- GEM39:
-   Raw_Arr_Size : constant Stream_Element_Offset :=
-                                 Ada.Streams.Stream_Element_Offset(NBits) / Stream_Element'Size;
-   type SEA_Pointer is access all Stream_Element_Array (1 .. Raw_Arr_Size);
-   function As_SEA_Pointer is new Ada.Unchecked_Conversion (System.Address, SEA_Pointer);
-
-begin
-   Put(Integer'Image(NBits));
-   --Phys_To_Raw(Af, A,B, Phys_Arr);
-   -- low level write
-   Raw_CheckAndRevert(Af);
-   Ada.Streams.Write(S.all, As_SEA_Pointer(Af'Address).all );
-   --Ada.Streams.Stream_Element_Array'Write(S, As_SEA_Pointer(Af'Address).all );
---   Raw.Numeric_Arr'Write(S, Af);
-end Write;
-
 
 
 end Value;
--- lib/src/image/value.ads
+++ lib/src/image/value.ads
@@ -18,44 +18,5 @@ package Value is
     Scaling : Access_Rec;
     Phys_Arr : in Physical.Numeric_Arr);
 
-   procedure To_Sream_Elements_Array
-      (Raw_Arr : in Raw.Numeric_Arr;
-      SE_Arr : out Ada.Streams.Stream_Element_Array) is null;
-
-   procedure To_Raw_Numeric_Array
-      (Raw_Arr : out Raw.Numeric_Arr;
-      SE_Arr : in Ada.Streams.Stream_Element_Array) is null;
-
-
-      -- NOTE not really Read/Write but these could
-      -- substitute TT_AIO.Read/Write in Fits_IO.HDU_Write/Read:
-      -- has no Stream, but Stream compatible data(pointers!!)
-      -- (SEA is array of alised elements)
-procedure Read_From_SE_Array
-    (SE_Arr : in Ada.Streams.Stream_Element_Array;
-    A,B : in Float;
-    Phys_Arr : out Physical.Numeric_Arr) is null;
-
-procedure Write_To_SE_Array
-    (SE_Arr : out Ada.Streams.Stream_Element_Array;
-    A,B : in Float;
-    Phys_Arr : in Physical.Numeric_Arr)is null;
-
-
-
-
-
-   -- orig API
-
-procedure Read
-    (S : not null access Ada.Streams.Root_Stream_Type'Class; 
-    A,B : in Float;
-    Phys_Arr : out Physical.Numeric_Arr);
-
-procedure Write
-    (S : not null access Ada.Streams.Root_Stream_Type'Class;
-    A,B : in Float;
-    Phys_Arr : in Physical.Numeric_Arr);
-
 end Value;
 
